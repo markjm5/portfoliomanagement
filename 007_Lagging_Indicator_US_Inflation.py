217d9d7b8a3dc8bc7c9cf0c22f3e061905324025
markjm5 <mark.jm5@gmail.com>
2021-11-28 08:53:31 +0800

    moved ff target rate not merging correctly

--- 007_Lagging_Indicator_US_Inflation.py
+++ 007_Lagging_Indicator_US_Inflation.py
@@ -22,12 +22,26 @@ def get_newyorkfed_target_rate(type, dimensions):
     datetime_object = datetime.strptime(json["refRates"][i]['effectiveDate'], '%Y-%m-%d')
 
     #Check if current date is the first of the month. Only if it is, add the row to the dataframe. 
-    if datetime_object.day == 1:
-      # TODO: format datetime so that it matches excel file.   
-      try:
-        df = df.append({"DATE": datetime_object.strftime("%Y-%m-%d"), dimensions[0].upper(): json["refRates"][i]['targetRateTo']}, ignore_index=True)
-      except KeyError as e:
-        df = df.append({"DATE": datetime_object.strftime("%Y-%m-%d"), dimensions[0].upper(): json["refRates"][i]['targetRateFrom']}, ignore_index=True)
+    #TODO: In cases where there is no first day of month, get the first day of the month where a FF target was released
+    #TODO: format datetime so that it matches excel file (ie. 1st day of every month).   
+    #if datetime_object.day == 1:
+    try:
+        df = df.append({"DATE": datetime_object.strftime("%Y-%m-1"), dimensions[0].upper(): str(json["refRates"][i]['targetRateTo'])}, ignore_index=True)
+    except KeyError as e:
+        df = df.append({"DATE": datetime_object.strftime("%Y-%m-1"), dimensions[0].upper(): str(json["refRates"][i]['targetRateFrom'])}, ignore_index=True)
+
+
+    df = df.drop_duplicates(subset=['DATE'],keep='first')
+
+    #else:
+    #    if datetime_object.year != year_set and datetime_object.month != month_set:
+    #        try:
+    #            df = df.append({"DATE": datetime_object.strftime("%Y-%m-1"), dimensions[0].upper(): json["refRates"][i]['targetRateTo']}, ignore_index=True)
+    #        except KeyError as e:
+    #            df = df.append({"DATE": datetime_object.strftime("%Y-%m-1"), dimensions[0].upper(): json["refRates"][i]['targetRateFrom']}, ignore_index=True)
+
+    #        year_set = datetime_object.year
+    #        month_set = datetime_object.month
 
   return df  
 
@@ -35,6 +49,7 @@ df_CPIAUCSL = get_stlouisfed_data('CPIAUCSL')
 df_CPIENGSL = get_stlouisfed_data('CPIENGSL')
 df_CPIFABSL = get_stlouisfed_data('CPIFABSL')
 df_CPILFESL = get_stlouisfed_data('CPILFESL')
+
 df_FEDFUNDS = get_stlouisfed_data('FEDFUNDS')
 
 #Get Fed Funds Target from markets.newyorkfed.org
@@ -42,18 +57,19 @@ df_FEDFUNDS = get_stlouisfed_data('FEDFUNDS')
 dataset = 'effr'
 startDate = '01/01/1971'
 endDate = '11/01/2021'
-df_get_ff_target_rate = get_newyorkfed_target_rate('unsecured', [dataset,startDate,endDate])
-
-#import pdb; pdb.set_trace()
+df_ff_target_rate = get_newyorkfed_target_rate('unsecured', [dataset,startDate,endDate])
 
 #Combine all these data frames into a single data frame based on the DATE field
 df = pd.merge(df_CPIAUCSL,df_CPIENGSL,"left")
 df = pd.merge(df,df_CPIFABSL,"left")
 df = pd.merge(df,df_CPILFESL,"left")
 df = pd.merge(df,df_FEDFUNDS,"left")
-df = pd.merge(df,df_get_ff_target_rate,"left")
+
+#TODO: Merge Not Working
+df = pd.merge(df,df_ff_target_rate,"left")
 
 #Write to a csv file in the correct directory
 write_to_directory(df,'007_Lagging_Indicator_US_Inflation.csv')
+
 
 print("Done!")
