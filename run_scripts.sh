e6897a27ee18535bfa7f4470c139d211852d58e3
markjm5 <mark.jm5@gmail.com>
2021-11-28 13:36:59 +0800

    updated bash script

--- run_scripts.sh
+++ run_scripts.sh
@@ -5,3 +5,4 @@ python 003_Lagging_Indicator_World_GDP.py
 python 005_Lagging_Indicator_Job_Market_US_World.py
 python 006_Lagging_Indicator_Personal_Consumption_Expenditures.py
 python 007_Lagging_Indicator_US_Inflation.py
+python 008_Lagging_Indicator_Global_Inflation.py
