820e2931396cfb93167e4aa43c321d0967e5bd96
markjm5 <mark.jm5@gmail.com>
2021-12-05 10:28:32 +0800

    updates to initial functions for scraping additional production tables for 010

--- 010_World_Industrial_Production.py
+++ 010_World_Industrial_Production.py
@@ -31,13 +31,17 @@ def scrape_table_world_production(url):
   soup = BeautifulSoup(page.content, 'html.parser')
 
   #TODO: Need to scrape table for world production countries and numbers.
-  value = soup.find('input', {'id': 'export_data'}).get('value')
-  countries = value.split('=')[4].split('&')[0]
-  numbers = value.split('=')[3].split('&')[0]
+  table = soup.find('table')
+  #import pdb; pdb.set_trace()
+  table_rows = table.find_all('tr', attrs={'align':'center'})
+  table_rows_header = table.find_all('tr')[0].find_all('th')
+  df = pd.DataFrame()
+  index = 0
+  for header in table_rows_header:
+    df.insert(index,str(header.text).strip(),[],True)
+    index+=1
 
-  #Need to put countries and numbers into a pandas dataframe.
-  dataframe_values = {'COUNTRY': countries.split('|'), 'CAPITAL_INVESTMENT': numbers.split('|')}
-  df = pd.DataFrame(data=dataframe_values)
+  #import pdb; pdb.set_trace()
 
   return df
 
@@ -48,13 +52,10 @@ def scrape_table_china_production(url):
   soup = BeautifulSoup(page.content, 'html.parser')
 
   #TODO: Need to scrape table for china production and numbers.
-  value = soup.find('input', {'id': 'export_data'}).get('value')
-  countries = value.split('=')[4].split('&')[0]
-  numbers = value.split('=')[3].split('&')[0]
-
-  #Need to put countries and numbers into a pandas dataframe.
-  dataframe_values = {'COUNTRY': countries.split('|'), 'CAPITAL_INVESTMENT': numbers.split('|')}
-  df = pd.DataFrame(data=dataframe_values)
+  table = soup.find('table')
+  table_rows = table.find_all('tr', attrs={'align':'center'})
+  table_rows_header = table.find_all('tr')[0].find_all('th')
+  df = pd.DataFrame()
 
   return df
 
@@ -65,14 +66,14 @@ df_capital_investment = scrape_div_capital_investment("https://www.theglobalecon
 write_to_directory(df_capital_investment,'010_Lagging_Indicator_Capital_Investment.csv')
 
 #Get World Production Data
-df_world_production = scrape_table_world_production("https://www.theglobaleconomy.com/rankings/Capital_investment/")
+df_world_production = scrape_table_world_production("https://tradingeconomics.com/country-list/industrial-production?continent=world")
 #Write to a csv file in the correct directory
 write_to_directory(df_world_production,'010_Lagging_Indicator_World_Production.csv')
 
 #Get China Production Data
-df_china_production = scrape_table_china_production("https://www.theglobaleconomy.com/rankings/Capital_investment/")
+#df_china_production = scrape_table_china_production("https://www.investing.com/economic-calendar/chinese-industrial-production-462")
 #Write to a csv file in the correct directory
-write_to_directory(df_china_production,'010_Lagging_Indicator_China_Production.csv')
+#write_to_directory(df_china_production,'010_Lagging_Indicator_China_Production.csv')
 
 
 # Get OECD Data Using API: https://stackoverflow.com/questions/40565871/read-data-from-oecd-api-into-python-and-pandas
