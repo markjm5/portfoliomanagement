d424f6871e960ae90462d222b9b1f8d7a107a622
markjm5 <mark.jm5@gmail.com>
2021-12-20 16:46:53 +0800

    updated 007. Still need to fill in gaps in EFFR and reorder columns to match excel

--- 007_Lagging_Indicator_US_Inflation.py
+++ 007_Lagging_Indicator_US_Inflation.py
@@ -2,8 +2,10 @@ import requests
 import os.path
 import csv
 import pandas as pd
-from common import get_stlouisfed_data, write_to_directory
+from common import get_stlouisfed_data, convert_excelsheet_to_dataframe, write_dataframe_to_excel, combine_df, write_to_directory
 from datetime import datetime
+
+excel_file_path = '/trading_excel_files/01_lagging_coincident_indicators/007_Lagging_Indicator_US_Inflation.xlsm'
 
 def get_newyorkfed_target_rate(type, dimensions):
 
@@ -27,10 +29,33 @@ def get_newyorkfed_target_rate(type, dimensions):
 
   return df  
 
+##################################
+#   Get Data from St Louis Fed   #
+##################################
+
+sheet_name = 'Database'
+
 df_CPIAUCSL = get_stlouisfed_data('CPIAUCSL')
 df_CPIENGSL = get_stlouisfed_data('CPIENGSL')
 df_CPIFABSL = get_stlouisfed_data('CPIFABSL')
 df_CPILFESL = get_stlouisfed_data('CPILFESL')
+
+#Combine all these data frames into a single data frame based on the DATE field
+df = pd.merge(df_CPIAUCSL,df_CPIENGSL,"left")
+df = pd.merge(df,df_CPIFABSL,"left")
+df = pd.merge(df,df_CPILFESL,"left")
+
+df_original = convert_excelsheet_to_dataframe(excel_file_path, sheet_name, True)
+
+df_updated = combine_df(df_original, df)
+
+write_dataframe_to_excel(excel_file_path, sheet_name, df_updated, False, 0)
+
+###################################################
+#   Get Data from St Louis Fed and New York Fed   #
+###################################################
+
+sheet_name = 'Fed Fund New'
 
 df_FEDFUNDS = get_stlouisfed_data('FEDFUNDS')
 #Get Fed Funds Target from markets.newyorkfed.org
@@ -40,16 +65,20 @@ startDate = '01/01/1971'
 endDate = '11/01/2021'
 df_ff_target_rate = get_newyorkfed_target_rate('unsecured', [dataset,startDate,endDate])
 
-#Combine all these data frames into a single data frame based on the DATE field
-df = pd.merge(df_CPIAUCSL,df_CPIENGSL,"left")
-df = pd.merge(df,df_CPIFABSL,"left")
-df = pd.merge(df,df_CPILFESL,"left")
-#df = pd.merge(df,df_FEDFUNDS,"left")
+df_ff_target_rate['DATE'] = pd.to_datetime(df_ff_target_rate['DATE'],format='%Y-%m-%d')
+df_ff_target_rate['EFFR'] = pd.to_numeric(df_ff_target_rate['EFFR'])
+
+#TODO: Fill in blanks in EFFR
 
 df_FEDFUNDS = df_FEDFUNDS.merge(df_ff_target_rate, how="left")
 
+#TODO: Rename and reorder columns
+
+
+write_dataframe_to_excel(excel_file_path, sheet_name, df_FEDFUNDS, False, 0)
+
 #Write to a csv file in the correct directory
-write_to_directory(df,'007_Lagging_Indicator_US_Inflation.csv')
-write_to_directory(df_FEDFUNDS,'007_Lagging_Indicator_US_Fed_Fund_Rate_Target.csv')
+#write_to_directory(df,'007_Lagging_Indicator_US_Inflation.csv')
+#write_to_directory(df_FEDFUNDS,'007_Lagging_Indicator_US_Fed_Fund_Rate_Target.csv')
 
 print("Done!")
