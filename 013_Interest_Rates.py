92c2bf214d55b9c7b91a43492cb56c54af015129
markjm5 <mark.jm5@gmail.com>
2022-01-01 00:55:51 +0800

    connection error in 013, have implemented time.sleep function. Need to test after a while whether this works

--- 013_Interest_Rates.py
+++ 013_Interest_Rates.py
@@ -11,6 +11,7 @@ from bs4 import BeautifulSoup
 from requests.models import parse_header_links
 import re
 import investpy
+import time
 from common import get_oecd_data, convert_excelsheet_to_dataframe, write_dataframe_to_excel, combine_df, write_to_directory, util_check_diff_list, scrape_world_gdp_table
 
 excel_file_path = '/Trading_Excel_Files/02_Interest_Rates_FX/013_Interest_Rates.xlsm'
@@ -18,7 +19,7 @@ excel_file_path = '/Trading_Excel_Files/02_Interest_Rates_FX/013_Interest_Rates.
 ###################################
 # Get Database 10y Data from OECD #
 ###################################
-
+"""
 country = ['AUS','AUT','BEL','CAN','CHL','CZE','DEU','DNK','ESP','EST','FIN','FRA','GBR','GRC','HUN','IRL','ISL','ISR','ITA','JPN','KOR','LUX','LVA','MEX','NLD','NOR','OECD','POL','PRT','SVK','SVN','SWE','USA','EA19','EU27_2020','G-7','CHE','IND','ZAF','RUS','CHN','TUR','BRA']
 subject = ['IRLTLT01']
 measure = ['ST']
@@ -34,10 +35,10 @@ df_db_10y = df_db_10y.drop('MTH', 1)
 sheet_name = 'Database 10y'
 df_original_db_10y = convert_excelsheet_to_dataframe(excel_file_path, sheet_name, True)
 
-"""
+
 # Check for difference between original and new lists
-print(Diff(df_db_10y.columns.tolist(), df_original_db_10y.columns.tolist()))
-"""
+#print(Diff(df_db_10y.columns.tolist(), df_original_db_10y.columns.tolist()))
+
 df_updated_db_10y = combine_df(df_original_db_10y, df_db_10y)
 
 # get a list of columns
@@ -55,14 +56,17 @@ df_updated_db_10y['DATE'] = pd.to_datetime(df_updated_db_10y['DATE'],format='%Y-
 write_dataframe_to_excel(excel_file_path, sheet_name, df_updated_db_10y, False, 0)
 
 print("Done!")
-
+"""
 ##############################################
 # Scrape 2y database Data from Investing.com #
 ##############################################
 
 # TODO: use below country list to get data and create df. Be mindful of order of countries, because it is used in 'Big Picture' sheet to load data
 # mexico = https://www.investing.com/rates-bonds/mexico-10-year-historical-data
-country_list = ['u.s.','canada','brazil','germany','france','italy','spain','portugal','netherlands','austria','greece','denmark','sweden','norway','switzerland','russia','turkey','poland','hungary','czech-republic','south-africa','japan','australia','singapore','china','hong-kong','india','indonesia','south-korea','philippines','thailand','vietnam','uk','new-zealand', 'mexico']
+#country_list = ['u.s.','canada','brazil','germany','france','italy','spain','portugal','netherlands','austria','greece','denmark','sweden','norway','switzerland','russia','turkey','poland','hungary','czech-republic','south-africa','japan','australia','singapore','china','hong-kong','india','indonesia','south-korea','philippines','thailand','vietnam','uk','new-zealand', 'mexico']
+
+country_list = ['argentina', 'australia', 'austria', 'bahrain', 'bangladesh', 'belgium', 'botswana', 'brazil', 'bulgaria', 'canada', 'chile', 'china', 'colombia', 'croatia', 'cyprus', 'czech republic', 'denmark', 'egypt', 'finland', 'france', 'germany', 'greece', 'hong kong', 'hungary', 'iceland', 'india', 'indonesia', 'ireland', 'israel', 'italy', 'japan', 'jordan', 'kenya', 'latvia', 'lithuania', 'luxembourg', 'malaysia', 'malta', 'mauritius', 'mexico', 'morocco', 'namibia', 'netherlands', 'new zealand', 'nigeria', 'norway', 'pakistan', 'peru', 'philippines', 'poland', 'portugal', 'qatar', 'romania', 'russia', 'saudi arabia', 'serbia', 'singapore', 'slovakia', 'slovenia', 'south africa', 'south korea', 'spain', 'sri lanka', 'sweden', 'switzerland', 'taiwan', 'thailand', 'turkey', 'uganda', 'ukraine', 'united kingdom', 'united states', 'venezuela', 'vietnam']
+
 # https://pypi.org/project/investpy/
 # df_example = investpy.get_stock_historical_data(stock='AAPL', country='United States', from_date='01/01/2010', to_date='01/01/2020')
 
@@ -74,7 +78,19 @@ for country in country_list:
         url = "https://www.investing.com/rates-bonds/%s-10-year-bond-yield-historical-data" % (country,)
     resp = requests.get(url=url)
     """
-    import pdb; pdb.set_trace()
+    bond = "%s %sy" % (country, '10')
+
+    try:
+        time.sleep(10)
+        data = investpy.get_bond_historical_data(bond=bond, from_date='01/01/2010', to_date='01/01/2019')
+        print(country)
+        print(data.head())
+
+    except RuntimeError as e:
+        print("%s does not exist" % (bond))
+
+
+import pdb; pdb.set_trace()
 
 ###############################################
 # Scrape 10y database Data from Investing.com #
