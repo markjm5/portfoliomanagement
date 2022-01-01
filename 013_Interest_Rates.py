735960ff4a39e6c6dc4dadef6be9bb40e4ead43c
markjm5 <mark.jm5@gmail.com>
2022-01-01 20:56:21 +0800

    troubleshooting 013 corrupt file

--- 013_Interest_Rates.py
+++ 013_Interest_Rates.py
@@ -64,18 +64,22 @@ write_dataframe_to_excel(excel_file_path, sheet_name, df_updated_db_10y, False, 
 # mexico = https://www.investing.com/rates-bonds/mexico-10-year-historical-data
 #country_list = ['u.s.','canada','brazil','germany','france','italy','spain','portugal','netherlands','austria','greece','denmark','sweden','norway','switzerland','russia','turkey','poland','hungary','czech-republic','south-africa','japan','australia','singapore','china','hong-kong','india','indonesia','south-korea','philippines','thailand','vietnam','uk','new-zealand', 'mexico']
 
-sheet_name = '10y database NEW'
-df_original_invest_10y = convert_excelsheet_to_dataframe(excel_file_path, sheet_name, True, None,'%b %d, %Y')
+sheet_name = '10y Database New'
+df_original_invest_10y = convert_excelsheet_to_dataframe(excel_file_path, sheet_name, True, None,'%d/%m/%Y')
 
-country_list = ['u.s.','canada','brazil','mexico','germany','france','italy','spain','portugal','netherlands','austria','greece','denmark','sweden','norway','switzerland', 'u.k.','russia','turkey','poland','hungary','czech republic','south africa','japan','australia','new zealand','singapore','china','hong kong','india','indonesia','south korea','philippines','thailand','vietnam']
-#country_list = ['u.s.','canada']
+#df_original_invest_10y = convert_excelsheet_to_dataframe(excel_file_path, sheet_name, True, None,'%b %d, %Y')
+#write_dataframe_to_excel(excel_file_path, sheet_name, df_original_invest_10y, False, 0)
+
+#country_list = ['u.s.','canada','brazil','mexico','germany','france','italy','spain','portugal','netherlands','austria','greece','denmark','sweden','norway','switzerland', 'u.k.','russia','turkey','poland','hungary','czech republic','south africa','japan','australia','new zealand','singapore','china','hong kong','india','indonesia','south korea','philippines','thailand','vietnam']
+country_list = ['u.s.', 'canada'] 
+
+#TODO: Works with US and Canada only, but when we add additional cols it falls apart. Use 013_Interest_Rates_working2 to continue testing
+
 #country_missing = ['denmark','sweden']
 df_invest_10y = get_invest_data(country_list, '10', '28/12/2000')
 
 #TODO: make sure merging of new and original df results in similar data to original
 df_updated_invest_10y = combine_df_on_index(df_original_invest_10y, df_invest_10y, 'DATE')
-
-import pdb; pdb.set_trace()
 
 # get a list of columns
 cols = list(df_updated_invest_10y)
@@ -86,11 +90,11 @@ cols.insert(0, cols.pop(cols.index('DATE')))
 df_updated_invest_10y = df_updated_invest_10y[cols]
 
 # format date
-df_updated_invest_10y['DATE'] = pd.to_datetime(df_updated_invest_10y['DATE'],format='%b %d, %Y')
-
+#df_updated_invest_10y['DATE'] = pd.to_datetime(df_updated_invest_10y['DATE'],format='%b %d, %Y')
+#import pdb; pdb.set_trace()
 # Write the updated df back to the excel sheet
 write_dataframe_to_excel(excel_file_path, sheet_name, df_updated_invest_10y, False, 0)
-
+"""
 ###########################################
 # Get 2y database Data from Investing.com #
 ###########################################
@@ -99,16 +103,18 @@ write_dataframe_to_excel(excel_file_path, sheet_name, df_updated_invest_10y, Fal
 # mexico = https://www.investing.com/rates-bonds/mexico-10-year-historical-data
 #country_list = ['u.s.','canada','brazil','germany','france','italy','spain','portugal','netherlands','austria','greece','denmark','sweden','norway','switzerland','russia','turkey','poland','hungary','czech-republic','south-africa','japan','australia','singapore','china','hong-kong','india','indonesia','south-korea','philippines','thailand','vietnam','uk','new-zealand', 'mexico']
 
+sheet_name = '2y Database New'
+df_original_invest_2y = convert_excelsheet_to_dataframe(excel_file_path, sheet_name, True, None,'%b %d, %Y')
+#df_original_invest_2y = convert_excelsheet_to_dataframe(excel_file_path, sheet_name, True, None,'%d/%m/%Y')
+write_dataframe_to_excel(excel_file_path, sheet_name, df_original_invest_2y, False, 0)
+
 country_list = ['u.s.','canada','brazil','mexico','germany','france','italy','spain','portugal','netherlands','austria','greece','denmark','sweden','norway','switzerland', 'u.k.','russia','turkey','poland','hungary','czech republic','south africa','japan','australia','new zealand','singapore','china','hong kong','india','indonesia','south korea','philippines','thailand','vietnam']
 #country_missing = ['denmark','sweden', 'mexico', 'greece', 'hungary', 'indonesia']
 
 df_invest_2y = get_invest_data(country_list, '2', '28/12/2000')
 #df_invest_2y = get_invest_data(country_missing, '2', '28/12/2000')
 
-sheet_name = '2y database NEW'
-df_original_invest_2y = convert_excelsheet_to_dataframe(excel_file_path, sheet_name, True, None,'%b %d, %Y')
-
-df_updated_invest_2y = combine_df(df_original_invest_2y, df_invest_2y)
+df_updated_invest_2y = combine_df_on_index(df_original_invest_2y, df_invest_2y, 'DATE')
 
 # get a list of columns
 cols = list(df_updated_invest_2y)
@@ -119,9 +125,9 @@ cols.insert(0, cols.pop(cols.index('DATE')))
 df_updated_invest_2y = df_updated_invest_2y[cols]
 
 # format date
-df_updated_invest_2y['DATE'] = pd.to_datetime(df_updated_invest_2y['DATE'],format='%b %d, %Y')
+#df_updated_invest_2y['DATE'] = pd.to_datetime(df_updated_invest_2y['DATE'],format='%b %d, %Y')
 
 # Write the updated df back to the excel sheet
 write_dataframe_to_excel(excel_file_path, sheet_name, df_updated_invest_2y, False, 0)
-
+"""
 print("Done!")
