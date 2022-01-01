7e86a5946ce783e215ba15ac33df6898d7b130a9
markjm5 <mark.jm5@gmail.com>
2022-01-01 15:29:47 +0800

    updates to 10y data in 013, created new common function to combine df by index. Added reset_index feature to function

--- common.py
+++ common.py
@@ -359,5 +359,5 @@ def combine_df_on_index(df1, df2, index_col):
   df1 = df1.set_index(index_col)
   df2 = df2.set_index(index_col)
 
-  return df1.combine_first(df2)
+  return df1.combine_first(df2).reset_index()
 
