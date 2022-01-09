ceafc33331de09a2dce4ba153a4d88f230c1c197
markjm5 <mark.jm5@gmail.com>
2022-01-09 14:18:30 +0800

    updated 014 py with params for oecd money supply data. needs testing

--- 014_US_Global_Money_Supply.py
+++ 014_US_Global_Money_Supply.py
@@ -141,40 +141,41 @@ write_dataframe_to_excel(excel_file_path, sheet_name, df_updated, False, -1)
 
 #TODO: Get data from correct OECD time series
 #TODO: Update excel sheet to remove unnecessary columns
+# https://stats.oecd.org/restsdmx/sdmx.ashx/GetData/MEI_FIN/MABM.AUS+CAN+CHL+COL+CZE+DNK+HUN+ISL+ISR+JPN+KOR+MEX+NZL+NOR+POL+SWE+CHE+TUR+GBR+USA+EA19+OECD+NMEC+BRA+CHN+IND+IDN+RUS+ZAF.M/all?startTime=2018&endTime=2021
 import pdb; pdb.set_trace()
 
 country = ['AUS','AUT','BEL','CAN','CHL','CZE','DEU','DNK','ESP','EST','FIN','FRA','GBR','GRC','HUN','IRL','ISL','ISR','ITA','JPN','KOR','LUX','LVA','MEX','NLD','NOR','OECD','POL','PRT','SVK','SVN','SWE','USA','EA19','EU27_2020','G-7','CHE','IND','ZAF','RUS','CHN','TUR','BRA']
-subject = ['IRLTLT01']
-measure = ['ST']
+subject = ['MABM']
+measure = ['ST'] #****Does not seem to exist****
 frequency = 'M'
 startDate = '1950-Q1'
 
 todays_date = date.today()
 endDate = '%s-Q4' % (todays_date.year)
 
-df_db_10y = get_oecd_data('KEI', [country, subject, measure, [frequency]], {'startTime': startDate, 'endTime': endDate, 'dimensionAtObservation': 'AllDimensions','filename': '013_Interest_Rates.xml'})
-df_db_10y = df_db_10y.drop('MTH', 1)
+df_money_supply = get_oecd_data('MEI_FIN', [country, subject, measure, [frequency]], {'startTime': startDate, 'endTime': endDate, 'dimensionAtObservation': 'AllDimensions','filename': '014_US_Global_Money_Supply.xml'})
+df_money_supply = df_money_supply.drop('MTH', 1)
 
 sheet_name = 'Data'
-df_original_db_10y = convert_excelsheet_to_dataframe(excel_file_path, sheet_name, True)
+df_original_money_supply = convert_excelsheet_to_dataframe(excel_file_path, sheet_name, True)
 
 # Check for difference between original and new lists
-#print(Diff(df_db_10y.columns.tolist(), df_original_db_10y.columns.tolist()))
+#print(Diff(df_money_supply.columns.tolist(), df_original_money_supply.columns.tolist()))
 
-df_updated_db_10y = combine_df(df_original_db_10y, df_db_10y)
+df_updated_money_supply = combine_df(df_original_money_supply, df_money_supply)
 
 # get a list of columns
-cols = list(df_updated_db_10y)
+cols = list(df_updated_money_supply)
 # move the column to head of list using index, pop and insert
 cols.insert(0, cols.pop(cols.index('DATE')))
 
 # reorder
-df_updated_db_10y = df_updated_db_10y[cols]
+df_updated_money_supply = df_updated_money_supply[cols]
 
 # format date
-df_updated_db_10y['DATE'] = pd.to_datetime(df_updated_db_10y['DATE'],format='%Y-%m-%d')
+df_updated_money_supply['DATE'] = pd.to_datetime(df_updated_money_supply['DATE'],format='%Y-%m-%d')
 
 # Write the updated df back to the excel sheet
-write_dataframe_to_excel(excel_file_path, sheet_name, df_updated_db_10y, False, 0)
+write_dataframe_to_excel(excel_file_path, sheet_name, df_updated_money_supply, False, 0)
 
 print("Done!")
