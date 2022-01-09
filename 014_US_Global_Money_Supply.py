b3bc673753d999defff3064c66b82b8433c7d0b0
markjm5 <mark.jm5@gmail.com>
2022-01-09 20:35:45 +0800

    working feature to get oecd data for 014, need to update excel doc

--- 014_US_Global_Money_Supply.py
+++ 014_US_Global_Money_Supply.py
@@ -139,10 +139,8 @@ write_dataframe_to_excel(excel_file_path, sheet_name, df_updated, False, -1)
 #TODO: Get data from correct OECD time series
 #TODO: Update excel sheet to remove unnecessary columns
 # https://stats.oecd.org/restsdmx/sdmx.ashx/GetData/MEI_FIN/MABM.AUS+CAN+CHL+COL+CZE+DNK+HUN+ISL+ISR+JPN+KOR+MEX+NZL+NOR+POL+SWE+CHE+TUR+GBR+USA+EA19+OECD+NMEC+BRA+CHN+IND+IDN+RUS+ZAF.M/all?startTime=2018&endTime=2021
-# https://stats.oecd.org/restsdmx/sdmx.ashx/GetData/MEI_FIN/AUS+BEL+BRA+CAN+CHE+CHL+CHN+COL+CRI+CZE+DEU+DNK+EA19+ESP+EST+EU27_2020+FIN+FRA+G-20+G-7+GBR+GRC+HUN+IDN+IND+IRL+ISL+ISR+ITA+JPN+KOR+LTU+LUX+LVA+MEX+NLD+NOR+OECD+OECDE+POL+PRT+RUS+SAU+SVK+SVN+SWE+TUR+USA+ZAF+NZL.MABM.M/all?startTime=1951-Q1&endTime=2022-Q4
-# No EU27_2020, G-20, G-7
 
-country = ['AUS','BEL','BRA','CAN','CHE','CHL','CHN','COL','CRI','CZE','DEU','DNK','EA19','ESP','EST','FIN','FRA','GBR','GRC','HUN','IDN','IND','IRL','ISL','ISR','ITA','JPN','KOR','LTU','LUX','LVA','MEX','NLD','NOR','OECD','OECDE','POL','PRT','RUS','SAU','SVK','SVN','SWE','TUR','USA','ZAF','NZL']
+country = ['AUS','BRA','CAN','CHE','CHL','CHN','COL','CRI','CZE','DNK','EA19','GBR','HUN','IDN','IND','ISL','ISR','JPN','KOR','MEX','NOR','OECD','OECDE','POL','RUS','SWE','TUR','USA','ZAF','NZL']
 subject = ['MABM']
 measure = []
 frequency = 'M'
@@ -153,14 +151,16 @@ endDate = '%s-Q4' % (todays_date.year)
 
 df_money_supply = get_oecd_data('MEI_FIN', [country, subject, measure, [frequency]], {'startTime': startDate, 'endTime': endDate, 'dimensionAtObservation': 'AllDimensions','filename': '014_US_Global_Money_Supply.xml'})
 df_money_supply = df_money_supply.drop('MTH', 1)
-import pdb; pdb.set_trace()
+
 sheet_name = 'Data'
 df_original_money_supply = convert_excelsheet_to_dataframe(excel_file_path, sheet_name, True)
 
 # Check for difference between original and new lists
-#print(Diff(df_money_supply.columns.tolist(), df_original_money_supply.columns.tolist()))
+#util_check_diff_list(df_money_supply.columns.tolist(), df_original_money_supply.columns.tolist())
+#util_check_diff_list(df_original_money_supply.columns.tolist(),df_money_supply.columns.tolist())
 
-df_updated_money_supply = combine_df(df_original_money_supply, df_money_supply)
+#df_updated_money_supply = combine_df(df_original_money_supply, df_money_supply)
+df_updated_money_supply = combine_df_on_index(df_original_money_supply, df_money_supply, 'DATE')
 
 # get a list of columns
 cols = list(df_updated_money_supply)
@@ -172,8 +172,6 @@ df_updated_money_supply = df_updated_money_supply[cols]
 
 # format date
 df_updated_money_supply['DATE'] = pd.to_datetime(df_updated_money_supply['DATE'],format='%Y-%m-%d')
-
-import pdb; pdb.set_trace()
 
 # Write the updated df back to the excel sheet
 write_dataframe_to_excel(excel_file_path, sheet_name, df_updated_money_supply, False, 0)
