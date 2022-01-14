00cc591de6f43f62357a8b91fcc824e2a6031134
markjm5 <mark.jm5@gmail.com>
2022-01-14 22:42:04 +0800

    added todo notes to 016

--- 016_Leading_Indicator_US_ISM_Manufacturing.py
+++ 016_Leading_Indicator_US_ISM_Manufacturing.py
@@ -19,4 +19,12 @@ from common import combine_df_on_index, get_stlouisfed_data, get_yf_data
 excel_file_path = '/Trading_Excel_Files/03_Leading_Indicators/016_Leading_Indicator_US_ISM_Manufacturing.xlsm'
 sheet_name = 'DB ISM'
 
+#TODO: Get ISM data for the following tabs:
+#Sectors Trend
+#Details
+#ISM Manufacturing vs GDP
+#ISM Manufacturing vs SPX
+#ISM Manufacturing
+#Industry Comments ?
+
 print("Done!")
