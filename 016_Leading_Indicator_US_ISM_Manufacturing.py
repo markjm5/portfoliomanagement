fb31919484c75bc4b71eff5a68bcba1afb16a0bd
markjm5 <mark.jm5@gmail.com>
2022-01-14 22:37:40 +0800

    created initial 016 py and excel files

---
+++ 016_Leading_Indicator_US_ISM_Manufacturing.py
@@ -0,0 +1,22 @@
+from json.decoder import JSONDecodeError
+import sys
+import requests
+import os.path
+import csv
+import calendar
+import re
+import pandas as pd
+import xml.etree.ElementTree as ET
+from inspect import getmembers, isclass, isfunction
+from datetime import datetime as dt
+from dateutil import parser, relativedelta
+from datetime import date
+from bs4 import BeautifulSoup
+from requests.models import parse_header_links
+from common import get_oecd_data, get_invest_data, convert_excelsheet_to_dataframe, write_dataframe_to_excel, combine_df, util_check_diff_list
+from common import combine_df_on_index, get_stlouisfed_data, get_yf_data
+
+excel_file_path = '/Trading_Excel_Files/03_Leading_Indicators/016_Leading_Indicator_US_ISM_Manufacturing.xlsm'
+sheet_name = 'DB ISM'
+
+print("Done!")
