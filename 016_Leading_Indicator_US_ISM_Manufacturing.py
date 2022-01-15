f774b0a746637f7c7185c9327eab1cbd777f78e5
markjm5 <mark.jm5@gmail.com>
2022-01-15 10:08:09 +0800

    added initial ism page scraping function

--- 016_Leading_Indicator_US_ISM_Manufacturing.py
+++ 016_Leading_Indicator_US_ISM_Manufacturing.py
@@ -19,6 +19,24 @@ from common import combine_df_on_index, get_stlouisfed_data, get_yf_data
 excel_file_path = '/Trading_Excel_Files/03_Leading_Indicators/016_Leading_Indicator_US_ISM_Manufacturing.xlsm'
 sheet_name = 'DB ISM'
 
+def scrape_pmi_manufacturing_index():
+
+    #get date range
+    todays_date = date.today()
+
+    #use todays date to get pmi month (last month) and use the month string to call the url
+    pmi_date = todays_date - relativedelta.relativedelta(months=1)
+    pmi_month = pmi_date.strftime("%B").lower()
+    url_pmi = 'https://www.ismworld.org/supply-management-news-and-reports/reports/ism-report-on-business/pmi/%s' % (pmi_month,)
+
+    page = requests.get(url=url_pmi,verify=False)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    import pdb; pdb.set_trace()
+
+
+scrape_pmi_manufacturing_index()
+
 #TODO: Get ISM data for the following tabs:
 #Sectors Trend
 #Details
