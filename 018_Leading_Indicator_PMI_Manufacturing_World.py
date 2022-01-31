61236df122f8b158d7ac02f3b25b5be94265d4c0
markjm5 <mark.jm5@gmail.com>
2022-01-31 16:05:20 +0800

    added initial extract_countries_pmi function to 018 py file

--- 018_Leading_Indicator_PMI_Manufacturing_World.py
+++ 018_Leading_Indicator_PMI_Manufacturing_World.py
@@ -19,19 +19,84 @@ from common import convert_excelsheet_to_dataframe, write_dataframe_to_excel
 from common import combine_df_on_index, convert_html_table_to_df, _util_check_diff_list, _transform_data
 
 excel_file_path = '/Trading_Excel_Files/03_Leading_Indicators/018_Temp.xlsm'
+sheet_name = 'DB Country PMI'
 
-data = {'Date': []}
- 
-# Convert the dictionary into DataFrame
-df_countries_pmi = pd.DataFrame(data)
+#get date range
+todays_date = date.today()
 
-sheet_name_original = 'DB Country PMI'
+#use todays date to get pmi month (first day of last month) and use the date in scraping functions
+pmi_date = todays_date - relativedelta.relativedelta(months=1)
+pmi_date = "01-%s-%s" % (pmi_date.month, pmi_date.year) #make the pmi date the first day of pmi month
+pmi_date = dt.strptime(pmi_date, "%d-%m-%Y")
+pmi_month = pmi_date.strftime("%B")
+
+#url_pmi = 'https://www.ismworld.org/supply-management-news-and-reports/reports/ism-report-on-business/services/%s' % (pmi_month.lower(),)
+
+def extract_countries_pmi(pmi_date):
+
+    data = {'Date': []}
+    
+    # Convert the dictionary into DataFrame
+    df_countries_pmi = pd.DataFrame(data)
+
+    countries = ['Global','US','EuroArea','Japan','Germany','France','UK','Italy','Spain','Brazil'
+                ,'Mexico','Russia','India','Canada','Australia','Indonesia','SouthKorea','Taiwan','Greece','Ireland',
+                'Turkey','CzechRepublic','Poland','Denmark','Vietnam','Thailand','SouthAfrica','HongKong','SaudiArabia','NewZealand']
+
+    for country in countries:
+        url_pmi = "https://tradingeconomics.com/%s/business-confidence" % (country)
+
+        page = requests.get(url=url_pmi,verify=False)
+
+        #TODO: Extract Date and PMI
+        #TODO: Add Date and PMI to df_countries_pmi
+
+
+        print(country)
+
+
+    #TODO: Scrape The FOllowing:
+    #https://tradingeconomics.com/china/business-confidence
+    #https://tradingeconomics.com/australia/indicators
+    #https://tradingeconomics.com/china/indicators
+    #https://tradingeconomics.com/japan/indicators
+    #https://tradingeconomics.com/germany/indicators
+    #https://tradingeconomics.com/france/indicators
+    #https://tradingeconomics.com/united-kingdom/indicators
+    #https://tradingeconomics.com/italy/indicators
+    #https://tradingeconomics.com/spain/indicators
+    #https://tradingeconomics.com/brazil/indicators
+    #https://tradingeconomics.com/mexico/indicators
+    #https://tradingeconomics.com/russia/indicators
+    #https://tradingeconomics.com/india/indicators
+    #https://tradingeconomics.com/canada/indicators
+    #https://tradingeconomics.com/indonesia/indicators
+    #https://tradingeconomics.com/south-korea/indicators
+    #https://tradingeconomics.com/taiwan/indicators
+    #https://tradingeconomics.com/greece/indicators
+    #https://tradingeconomics.com/ireland/indicators
+    #https://tradingeconomics.com/turkey/indicators
+    #https://tradingeconomics.com/czech-republic/indicators
+    #https://tradingeconomics.com/poland/indicators
+    #https://tradingeconomics.com/denmark/indicators
+
+
+    return df_countries_pmi
+
+
+#Get Country Rankings
+df_countries_pmi = extract_countries_pmi(pmi_date)
 
 # Load original data from excel file into original df
-df_original = convert_excelsheet_to_dataframe(excel_file_path, sheet_name_original, False)
+df_original = convert_excelsheet_to_dataframe(excel_file_path, sheet_name, False)
 
+import pdb; pdb.set_trace()
+
+#Combine new data with original data
+df_updated = combine_df_on_index(df_original, df_countries_pmi, 'Date')
+
+"""
 #TODO: Preparation: Load data from each country tab from Cell A5 into dataframe, merge based on date, and write to new excel sheet
-
 countries = ['Global','US','EuroArea','Japan','Germany','France','UK','Italy','Spain','Brazil'
                 ,'Mexico','Russia','India','Canada','Australia','Indonesia','SouthKorea','Taiwan','Greece','Ireland',
                 'Turkey','CzechRepublic','Poland','Denmark','Vietnam','Thailand','SouthAfrica','HongKong','SaudiArabia','NewZealand']
@@ -46,35 +111,8 @@ for country in countries:
     #Combine new data with original data
     df_countries_pmi = combine_df_on_index(df_country_data, df_countries_pmi, 'Date')
 
-    #df_countries_pmi.append(df_updated, ignore_index=True)
-
     print(country)
+"""
 
-import pdb; pdb.set_trace()
-
-
-
-#TODO: Scrape The FOllowing:
-#https://tradingeconomics.com/china/business-confidence
-#https://tradingeconomics.com/australia/indicators
-#https://tradingeconomics.com/china/indicators
-#https://tradingeconomics.com/japan/indicators
-#https://tradingeconomics.com/germany/indicators
-#https://tradingeconomics.com/france/indicators
-#https://tradingeconomics.com/united-kingdom/indicators
-#https://tradingeconomics.com/italy/indicators
-#https://tradingeconomics.com/spain/indicators
-#https://tradingeconomics.com/brazil/indicators
-#https://tradingeconomics.com/mexico/indicators
-#https://tradingeconomics.com/russia/indicators
-#https://tradingeconomics.com/india/indicators
-#https://tradingeconomics.com/canada/indicators
-#https://tradingeconomics.com/indonesia/indicators
-#https://tradingeconomics.com/south-korea/indicators
-#https://tradingeconomics.com/taiwan/indicators
-#https://tradingeconomics.com/greece/indicators
-#https://tradingeconomics.com/ireland/indicators
-#https://tradingeconomics.com/turkey/indicators
-#https://tradingeconomics.com/czech-republic/indicators
-#https://tradingeconomics.com/poland/indicators
-#https://tradingeconomics.com/denmark/indicators
+# Write the updated df back to the excel sheet
+write_dataframe_to_excel(excel_file_path, sheet_name, df_countries_pmi, False, 0)
