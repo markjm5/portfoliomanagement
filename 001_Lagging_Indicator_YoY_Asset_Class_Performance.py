fd0a0d116f4f2b4bd53a5c6db5bbc75bfadb8078
markjm5 <mark.jm5@gmail.com>
2022-02-05 02:04:21 +0800

    updated comments on 001 py file

--- 001_Lagging_Indicator_YoY_Asset_Class_Performance.py
+++ 001_Lagging_Indicator_YoY_Asset_Class_Performance.py
@@ -16,7 +16,6 @@ from common import combine_df_on_index, get_yf_data
 
 excel_file_path = '/Trading_Excel_Files/01_Lagging_Coincident_Indicators/001_Lagging_Indicator_YoY_Asset_Class_Performance.xlsm'
 
-
 ####################################
 # Get Asset Class Data from YF.com #
 ####################################
@@ -33,13 +32,14 @@ df_etf_data = pd.DataFrame(data)
 todays_date = date.today()
 date_str = "%s-%s-%s" % (todays_date.year, todays_date.month, todays_date.day)
 
-etfs = [ 'RXI','XLP','XLY','XLE','XLF','XLV','XLI','XLK','XLB','XLRE','XTL','XLU','SPY','USO','QQQ','IWM','IBB','EEM','HYG','VNQ','MDY','SLY','EFA','TIP','AGG','DJP','BIL','GOLD','DXY']
+etfs = [ 'RXI','XLP','XLY','XLE','XLF','XLV','XLI','XLK','XLB','XLRE','XTL','XLU','SPY','USO','QQQ','IWM','IBB','EEM','HYG','VNQ','MDY','SLY','EFA','TIP','AGG','DJP','BIL','GC=F','DX-Y.NYB']
 
 #TODO: Get the following data from YF
 
 for etf in etfs:
     print("Getting Data For: %s" % (etf,))
     df_etf = get_yf_data(etf, "1d", "2000-12-28", date_str)
+
     #Remove unnecessary columns and rename columns
     df_etf = df_etf.drop(['Open', 'High', 'Low', 'Volume'], axis=1)
     df_etf = df_etf.rename(columns={"Close": etf})
@@ -48,7 +48,7 @@ for etf in etfs:
 
 
 
-#TODO: Calculate Annual Returns on each asset class
+#TODO: Calculate Annual Returns on each asset class. Look at 007, 013 for calculation examples
 import pdb; pdb.set_trace()
 
 
