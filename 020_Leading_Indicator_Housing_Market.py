064481a60dbdf6cf536c658122e80820be8d6377
markjm5 <mark.jm5@gmail.com>
2022-02-04 10:40:27 +0800

    added 020 py file

---
+++ 020_Leading_Indicator_Housing_Market.py
@@ -0,0 +1,33 @@
+import requests
+import os.path
+import csv
+import pandas as pd
+from common import get_stlouisfed_data, convert_excelsheet_to_dataframe, write_dataframe_to_excel
+from common import combine_df_on_index
+
+excel_file_path = '/Trading_Excel_Files/03_Leading_Indicators/020_Leading_Indicator_Housing_Market.xlsm'
+sheet_name = 'Database New'
+
+df_PERMIT = get_stlouisfed_data('PERMIT')
+df_HOUST = get_stlouisfed_data('HOUST')
+df_COMPUTSA = get_stlouisfed_data('COMPUTSA')
+
+#Combine all these data frames into a single data frame based on the DATE field
+
+df = combine_df_on_index(df_PERMIT,df_HOUST,"DATE")
+df = combine_df_on_index(df_COMPUTSA,df,"DATE")
+
+#LEGACY: Write to a csv file in the correct directory
+#write_to_directory(df,'002_Lagging_Indicator_US_GDP.csv')
+
+# Rather than writing to directory, update and save the sheet
+# Get Original Sheet and store it in a dataframe
+df_original = convert_excelsheet_to_dataframe(excel_file_path, sheet_name, True)
+#df_updated = append_new_rows_to_df(df_original, df, 'DATE')
+
+df_updated = combine_df_on_index(df_original, df, 'DATE')
+
+# Write the updated df back to the excel sheet
+write_dataframe_to_excel(excel_file_path, sheet_name, df_updated, False, 0)
+
+print("Done!")
