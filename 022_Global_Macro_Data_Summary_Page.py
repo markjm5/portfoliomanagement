0042a0392b17d0beffdc723d7aedb7874107925c
markjm5 <mark.jm5@gmail.com>
2022-02-07 10:21:17 +0800

    added time series for 022

--- 022_Global_Macro_Data_Summary_Page.py
+++ 022_Global_Macro_Data_Summary_Page.py
@@ -12,8 +12,22 @@ from requests.models import parse_header_links
 from common import convert_excelsheet_to_dataframe, write_dataframe_to_excel
 from common import combine_df_on_index, get_yf_data, get_gdp_fred,get_oecd_data
 from common import get_ism_manufacturing_content, scrape_ism_manufacturing_headline_index
+from common import get_stlouisfed_data
 
 excel_file_path = '/Trading_Excel_Files/03_Leading_Indicators/018_Leading_Indicator_PMI_Manufacturing_World.xlsm'
+sheet_name = 'DB Big Picture'
 
+#Get GDP
+df_GDPC1 = get_gdp_fred('GDPC1')
+#TODO: Get last GDP Number (QoQ, YoY). Then get GDP numbers for 6m and 12m ago from last
+
+#Get Core CPI
+df_CPILFESL = get_stlouisfed_data('CPILFESL')
+
+#Get Core PCE
+df_PCEPILFE = get_stlouisfed_data('PCEPILFE')
+
+#Retail Sales Ex Auto and Gas
+df_MARTSSM44W72USS = get_stlouisfed_data('MARTSSM44W72USS')
 
 print("Done!")
