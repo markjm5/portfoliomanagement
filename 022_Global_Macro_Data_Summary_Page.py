1dd303e2c3972db730793f5bb0f315a425a8ecb2
markjm5 <mark.jm5@gmail.com>
2022-02-09 23:30:27 +0800

    updated 022 with get_current_ffr_target function

--- 022_Global_Macro_Data_Summary_Page.py
+++ 022_Global_Macro_Data_Summary_Page.py
@@ -16,10 +16,86 @@ from common import get_stlouisfed_data
 
 excel_file_path = '/Trading_Excel_Files/03_Leading_Indicators/018_Leading_Indicator_PMI_Manufacturing_World.xlsm'
 
+def get_current_ffr_target():
+
+    url = "https://www.bankrate.com/rates/interest-rates/federal-funds-rate.aspx"
+
+    page = requests.get(url=url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    data = {'Date': []}
+    
+    # Convert the dictionary into DataFrame
+    df_countries_pmi = pd.DataFrame(data)
+
+    #TODO: Need to scrape table for world production countries and numbers.
+    table = soup.find('table')
+
+    ffr_str = table.find('tbody').find('tr').find('td').text.strip()
+
+    pattern_select = re.compile(r'([0-9\.\-]*(?=\)))')
+
+    arr_rate = pattern_select.findall(ffr_str)
+    current_ffr_target = ""
+
+    if(len(arr_rate) > 0):
+        current_ffr_target = arr_rate[0]
+
+    return current_ffr_target
+
+def get_eurodollar_futures():
+
+    url='https://www.cmegroup.com/markets/interest-rates/stirs/eurodollar.quotes.html'
+
+    page = requests.get(url=url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    table = soup.find('table')
+
+    import pdb; pdb.set_trace()
+    #table_rows = table.find_all('tr', attrs={'align':'center'})
+    #table_rows_header = table.find_all('tr')[0].find_all('th')
+    df = pd.DataFrame()
+
+    index = 0
+    for header in table_rows_header:
+        if(index == 0):
+            df.insert(0,"Country",[],True)
+        else:
+            df.insert(index,header.text,[],True)
+
+        index+=1
+
+    #Insert New Row. Format the data to show percentage as float
+
+    for tr in table_rows:
+        temp_row = []
+        first_col = True
+
+        td = tr.find_all('td')
+        for obs in td:
+            if(first_col):
+                text = ''.join(e for e in obs.text if e.isalnum())
+                text = re.sub("([A-Z])", " \\1", text).strip()
+            else:
+                if(obs.text.find('%') < 0):
+                    text = obs.text
+                else:
+                    text = obs.text.strip('%')
+                    text = float(text.strip('%'))/100
+
+        temp_row.append(text)        
+        first_col = False
+
+        df.loc[len(df.index)] = temp_row
+
+    return df    
+
+
 ############################################
 # Get US Lagging and Coincident Indicators #
 ############################################
-
+"""
 sheet_name = 'DB US Lagging Indicators'
 
 #US GDP
@@ -46,16 +122,22 @@ df_ICSA = get_stlouisfed_data('ICSA')
 
 #US Industrial Production
 df_INDPRO = get_stlouisfed_data('INDPRO')
-
+"""
 ##################################
 # Get US Rates and Currency Data #
 ##################################
 sheet_name = 'DB US Rates and Currency'
 
-#TODO: Get Fed Funds Current Target Rate From: https://www.bankrate.com/rates/interest-rates/federal-funds-rate.aspx
+#Get Current Fed Funds Current Target Rate
+
+current_ffr_target = get_current_ffr_target()
 
 #TODO: Calculate Eurodollar Futures quotes for 1m, 6m, 12m
 # https://www.cmegroup.com/markets/interest-rates/stirs/eurodollar.quotes.html
+
+df_eurodollar_futures = get_eurodollar_futures()
+
+import pdb; pdb.set_trace()
 
 #TODO: Get Bond Yiends for 30y, 10y, 2y, 3m, and yield curve (ie. 10y - 2y)
 
