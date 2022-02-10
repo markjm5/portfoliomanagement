e9e934488c3cb064337149b74b0bcc92f0ef2b6c
markjm5 <mark.jm5@gmail.com>
2022-02-10 13:03:29 +0800

    022 updates to get_eurodollar_futures function

--- 022_Global_Macro_Data_Summary_Page.py
+++ 022_Global_Macro_Data_Summary_Page.py
@@ -9,6 +9,7 @@ from dateutil import parser, relativedelta
 from datetime import date
 from helium import start_chrome
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 from requests.models import parse_header_links
@@ -50,61 +51,51 @@ def get_eurodollar_futures():
 
     url = "https://www.cmegroup.com/markets/interest-rates/stirs/eurodollar.quotes.html"
 
-    from selenium.webdriver.chrome.options import Options
     chrome_options = Options()
     chrome_options.add_argument("--headless")
+    chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows Phone 10.0; Android 4.2.1; Microsoft; Lumia 640 XL LTE) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Mobile Safari/537.36 Edge/12.10166")
 
-    driver = webdriver.Chrome(ChromeDriverManager().install())
-    #driver = webdriver.Chrome(ChromeDriverManager().install(),options=chrome_options)
-
-    #driver = webdriver.Chrome()
-    
+    driver = webdriver.Chrome(ChromeDriverManager().install(),options=chrome_options)
     driver.get(url)
     html = driver.page_source
     driver.close()
 
     soup = BeautifulSoup(html, 'html.parser')
-    table = soup.find_all('table')
+    table = soup.find('table')
 
-    import pdb; pdb.set_trace()
+    # Iterate through table elements and get eurodollar futures quotes
+    table_rows = table.find_all('tr')
+    table_rows_header = table.find_all('tr')[0].find_all('th')
 
-    #TODO: Iterate through table elements and get eurodollar futures quotes
-    #table_rows = table.find_all('tr', attrs={'align':'center'})
-    #table_rows_header = table.find_all('tr')[0].find_all('th')
     df = pd.DataFrame()
-
     index = 0
-    for header in table_rows_header:
-        if(index == 0):
-            df.insert(0,"Country",[],True)
-        else:
-            df.insert(index,header.text,[],True)
 
+    for header in table_rows_header:
+        df.insert(index,header.text,[],True)
         index+=1
 
     #Insert New Row. Format the data to show percentage as float
-
     for tr in table_rows:
         temp_row = []
         first_col = True
 
         td = tr.find_all('td')
-        for obs in td:
-            if(first_col):
-                text = ''.join(e for e in obs.text if e.isalnum())
-                text = re.sub("([A-Z])", " \\1", text).strip()
-            else:
-                if(obs.text.find('%') < 0):
-                    text = obs.text
-                else:
-                    text = obs.text.strip('%')
-                    text = float(text.strip('%'))/100
+        if(td):
 
-        temp_row.append(text)        
-        first_col = False
+            for obs in td:
+                text = obs.text
+                temp_row.append(text)        
 
-        df.loc[len(df.index)] = temp_row
+            df.loc[len(df.index)] = temp_row
 
+    df = df.drop(columns=['Change','Options','Chart','Open', 'High', 'Low', 'Volume', 'Updated', 'PriorSettle'], axis=1)
+
+    #TODO: Remove rows with '-'
+    #TODO: Format 'Last' as Numeric
+    #TODO: Format Month as Date
+    #TODO: Calculate Eurodollar Futures quotes for 1m, 6m, 12m
+
+    import pdb; pdb.set_trace()
     return df    
 
 
