e317f1bfd905f3ec252959fdf0d09cbe2a4d9d1a
markjm5 <mark.jm5@gmail.com>
2022-02-14 15:23:45 +0800

    updates to sh script to print execution duration time

--- sh_00_macro.sh
+++ sh_00_macro.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-SECONDS=0
-
 bash sh_04_all.sh 2>&1 | tee output.txt
-
-duration=$SECONDS
-echo "Total Time: $(($duration / 60)) minutes and $(($duration % 60)) seconds"
--- sh_04_all.sh
+++ sh_04_all.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+SECONDS=0
+
 bash ./sh_01_lagging_coincident_indicators.sh
 bash ./sh_02_central_banks_interest_rates.sh
 bash ./sh_03_leading_indicators.sh
+
+duration=$SECONDS
+echo "Total Time: $(($duration / 60)) minutes and $(($duration % 60)) seconds"
