9eb3ba9a576cdadc6f1f6845dabc251fb1a52241
markjm5 <mark.jm5@gmail.com>
2022-05-28 16:08:14 +0800

    cleaned up 030 file

--- 030_Filtering_Process_Stock_One_Pager.py
+++ 030_Filtering_Process_Stock_One_Pager.py
@@ -7,6 +7,10 @@ excel_file_path = '/Trading_Excel_Files/04_Filtering_Process/030_Filtering_Proce
 
 fmpcloud_account_key = '14afe305132a682a2742743df532707d'
 
+#################################
+# Get Aggregate Data for S&P500 #
+#################################
+
 sheet_name = 'Database S&P500'
 df_sp_500 = convert_excelsheet_to_dataframe(excel_file_path, sheet_name, True)
 
@@ -17,18 +21,36 @@ sp_index = '^GSPC'
 data = get_fmpcloud_data(url,'030_SP500_details.json')
 
 sp_price = ""
-
-#Ensure that whether loaded from file or api, we are able to get the price.
 for index in data:
     if index['symbol'] == '^GSPC':
         sp_price = index['price']
 
 print(sp_price)
 
+#TODO:
+# Earnings Per Share (Annual)
+# Price to Earnings (P/E) (Annual)
+# Dividend Yield	
+# Book Value per share
+# Calculate Price to Book (P/B)
+# Calculate Price to Sales (P/S)
 import pdb; pdb.set_trace()
+#################################################
+# Get Aggregate Data for Sectors and Industries #
+#################################################
+
+# Sectors PE Ratio: https://fmpcloud.io/api/v4/sector_price_earning_ratio?date=2021-05-07&exchange=NYSE&apikey=14afe305132a682a2742743df532707d
+# Industries PE Ratio: https://fmpcloud.io/api/v4/industry_price_earning_ratio?date=2021-05-07&exchange=NYSE&apikey=14afe305132a682a2742743df532707d
+
+################################################
+# Get Aggregate Data for Single Name Companies #
+################################################
 
 
-#All Indexes: https://fmpcloud.io/api/v3/quotes/index?apikey=14afe305132a682a2742743df532707d
+sheet_name = 'Database US Companies'
+df_us_companies = convert_excelsheet_to_dataframe(excel_file_path, sheet_name, True)
+
+import pdb; pdb.set_trace()
 
 #Winners and Losers:
 # https://fmpcloud.io/api/v3/actives?apikey=14afe305132a682a2742743df532707d
@@ -36,15 +58,8 @@ import pdb; pdb.set_trace()
 # https://fmpcloud.io/api/v3/gainers?apikey=14afe305132a682a2742743df532707d
 # https://fmpcloud.io/api/v3/sectors-performance?apikey=14afe305132a682a2742743df532707d
 
-# Industries PE Ratio: https://fmpcloud.io/api/v4/industry_price_earning_ratio?date=2021-05-07&exchange=NYSE&apikey=14afe305132a682a2742743df532707d
-# Sectors PE Ratio: https://fmpcloud.io/api/v4/sector_price_earning_ratio?date=2021-05-07&exchange=NYSE&apikey=14afe305132a682a2742743df532707d
-
-# Commitment of Traders: https://fmpcloud.io/api/v4/commitment_of_traders_report/ES?apikey=14afe305132a682a2742743df532707d
-# https://fmpcloud.io/api/v4/commitment_of_traders_report_analysis?from=2020-09-12&to=2021-01-01&apikey=14afe305132a682a2742743df532707d
-# https://fmpcloud.io/api/v4/commitment_of_traders_report_analysis/M6?apikey=14afe305132a682a2742743df532707d
-
 # Finwiz company quote: https://finviz.com/quote.ashx?t=CRM
-#Company Real Time Quote: https://fmpcloud.io/api/v3/quote/AAPL?apikey=14afe305132a682a2742743df532707d
+# Company Real Time Quote: https://fmpcloud.io/api/v3/quote/AAPL?apikey=14afe305132a682a2742743df532707d
 # Company Profile: https://fmpcloud.io/api/v3/profile/CRM?apikey=14afe305132a682a2742743df532707d
 # Company Key Metrics: https://fmpcloud.io/api/v3/key-metrics-ttm/CRM?limit=40&apikey=14afe305132a682a2742743df532707d
 # Financial Statements Growth: https://fmpcloud.io/api/v3/financial-growth/CRM?limit=20&apikey=14afe305132a682a2742743df532707d
@@ -146,126 +161,4 @@ ROE
 
 Historical Earnings Surprises
 """
-
-
-def get_fmpcloud_data(url):
-
-  dim_args = ['+'.join(d) for d in dimensions]
-  dim_str = '.'.join(dim_args).replace('..','.')
-
-  url = "https://fmpcloud.io/api/v3/stock-screener?sector={sector}&marketCapMoreThan={marketCap}&limit={limit}&apikey={demo_account_key}').json()"
-
-  file_path = "%s/XML/%s" % (sys.path[0],params['filename'])
-
-  resp = requests.get(url=url)
-
-  try:
-      resp.raise_for_status()
-  except requests.exceptions.HTTPError as e:
-      # Whoops it wasn't a 200
-
-      if(resp.status_code == 400):
-        #It didnt work with the original order of the params so lets try again
-        url = "https://stats.oecd.org/restsdmx/sdmx.ashx/GetData/%s/%s.%s.%s.%s/all?startTime=%s&endTime=%s" % (dataset, dim_args[1],dim_args[0],dim_args[2],dim_args[3],params['startTime'],params['endTime'])
-
-        #clean up any situation where the format of the url is broken
-        url = url.replace('..','.')
-
-        resp = get_page(url)
-
-      else:
-            # Whoops it wasn't a 200 
-            raise Exception("Http Response (%s) Is Not 200: %s" % (url,str(resp.status_code)))
-
-  resp_formatted = resp.text[resp.text.find('<'):len(resp.text)]
-  # Write response to an XML File
-  with open(file_path, 'w') as f:
-    f.write(resp_formatted)
-
-  # Load in the XML file into ElementTree
-  tree = ET.parse(file_path)
-
-  #Load into a dataframe and return the data frame
-  root = tree.getroot()
-
-  ns = {'sdmx': 'http://www.SDMX.org/resources/SDMXML/schemas/v2_0/generic'}
-
-  df = pd.DataFrame()
-
-  #Add column headers
-  df.insert(0,date_range,[],True)
-  df.insert(1,"DATE",[],True)
-  index = 2
-  for series in root.findall('./sdmx:DataSet/sdmx:Series',ns):
-    current_country = ""
-    for value in series.findall('./sdmx:SeriesKey/sdmx:Value',ns): 
-      if(value.get('concept')) == 'LOCATION':
-        current_country = value.get('value')
-        
-        df.insert(index,value.get('value'),[],True)
-    index+=1
-
-  # This needs to account for both Quarterly and Monthly data.
-  date_range_list = []
-  date_list = []
-  year_start, qtr_start =  params['startTime'].split('-Q')
-  year_end, qtr_end =  params['endTime'].split('-Q')
-
-  match date_range:
-    case 'QTR':
-      #From year_start to year_end, calculate all the quarters. Populate date_range_list and date_list
-      date_list = pd.date_range('%s-01-01' % (year_start),'%s-01-01' % (int(year_end)+1), freq='QS').strftime("1/%-m/%Y").tolist()
-      date_ranges = pd.PeriodIndex(pd.to_datetime(date_list, format='%d/%m/%Y'),freq='Q').strftime('%Y-Q%q')
-
-      date_range_list = date_ranges.tolist()
-
-      # Need to align QTR and DATE between df_original and df_QoQ.
-      date_list.pop(0)
-      date_range_list.pop()
-    case 'MTH':
-      #From year_start to year_end, calculate all the months. Populate date_range_list and date_list
-      date_range_list = pd.date_range('%s-01-01' % (year_start),'%s-01-01' % (int(year_end)+1), freq='MS').strftime("%Y-%m").tolist()
-      date_list = pd.date_range('%s-01-01' % (year_start),'%s-01-01' % (int(year_end)+1), freq='MS').strftime("1/%-m/%Y").tolist()
-
-  df[date_range] = date_range_list
-  df['DATE'] = date_list
-
-  #Add observations for all countries
-  for series in root.findall('./sdmx:DataSet/sdmx:Series',ns):
-    for value in series.findall('./sdmx:SeriesKey/sdmx:Value',ns): 
-      if(value.get('concept')) == 'LOCATION':
-        current_country = value.get('value')
-
-        observations = series.findall('sdmx:Obs',ns)
-
-        for observation in observations:
-          #Get qtr, date and observation. Add to column in chronological order for all countries
-
-          obs_qtr = observation.findall('sdmx:Time',ns)[0].text
-          obs_row = df.index[df[date_range] == obs_qtr].tolist()
-          obs_value = observation.findall('sdmx:ObsValue',ns)[0].get('value')
-
-          #match based on quarter and country, then add the observation value
-          df.loc[obs_row, current_country] = round(float(obs_value),9)        
-
-  #Set the date to the correct datatype, and ensure the format accounts for the correct positioning of day and month values
-  df['DATE'] = pd.to_datetime(df['DATE'],format='%d/%m/%Y')
-
-  print("Retrieved Data for Series %s" % (dataset,))
-
-  return df
-
-
-
-
-#if the api call was called today, get the output from the serialized pickle files. Otherwise, call the api to get the output
-if(api_loaded_today):
-    companies, balancesheet_list, incomestatemnt_list, cashflowstatemnt_list, marketcap_data_list, companydata_list, technological_companies = load_data_from_pickle()
-else:
-    companies, balancesheet_list, incomestatemnt_list, cashflowstatemnt_list, marketcap_data_list, companydata_list, technological_companies = load_data_from_api(sector, marketCap, limit, demo_account_key)
-
-
-
-
-
 print("Done!")
