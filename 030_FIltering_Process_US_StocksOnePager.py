3d935aa7396ce80e7aa7263c7e345216111f17eb
markjm5 <mark.jm5@gmail.com>
2022-06-11 17:29:00 +0800

    030 started work on single pager including getting data from nasdaq

--- 030_FIltering_Process_US_StocksOnePager.py
+++ 030_FIltering_Process_US_StocksOnePager.py
@@ -7,274 +7,105 @@ from dateutil.relativedelta import relativedelta
 from common import get_oecd_data, convert_excelsheet_to_dataframe, write_dataframe_to_excel
 from common import get_api_json_data,get_page, get_finwiz_stock_data, get_api_json_data_no_file, get_page_selenium, combine_df_on_index, write_value_to_cell_excel
 
-#################
-ticker = "AAPL" # COMPANY TICKER - CHANGE HERE
-#################
-
-fmpcloud_account_key = '14afe305132a682a2742743df532707d'
-
-temp_excel_file_path = '/Trading_Excel_Files/04_Filtering_Process/030_Filtering_Process_Quantitative_Analysis_US_Stocks.xlsm'
-temp_sheet_name = 'Database US Companies'
-
-#Get company data from various sources
-df_us_companies = convert_excelsheet_to_dataframe(temp_excel_file_path, temp_sheet_name, False)
-df_zacks_stock_data = df_us_companies[df_us_companies['TICKER'] == ticker]
-df_finwiz_stock_data = get_finwiz_stock_data(ticker)
-
-excel_file_path = '/Trading_Excel_Files/04_Filtering_Process/030_Filtering_Process_Quantitative_Analysis_US_StocksOnePager.xlsm'
-
-import pdb; pdb.set_trace()
-
-todays_date = date.today()
-one_year_ago = dt(todays_date.year - 1, 12, 31)
-two_year_ago = dt(todays_date.year - 2, 12, 31)
-three_year_ago = dt(todays_date.year - 3, 12, 31)
-
-list_dates = []
-list_dates.append(one_year_ago)
-list_dates.append(two_year_ago)
-list_dates.append(three_year_ago)
-
-#Get Company Specific Data
-df_company_details = df_us_companies.loc[df_us_companies['TICKER'] == ticker].reset_index(drop=True)
-
-#urls:
-url_finviz = "https://finviz.com/quote.ashx?t=%s" % (ticker)
-url_company_profile = "https://fmpcloud.io/api/v3/profile/%s?apikey=%s" % (ticker,fmpcloud_account_key)
-url_company_key_metrics = "https://fmpcloud.io/api/v3/key-metrics-ttm/%s?limit=40&apikey=%s"  % (ticker,fmpcloud_account_key)
-url_company_peers = "https://fmpcloud.io/api/v4/stock_peers?symbol=%s&apikey=%s"  % (ticker,fmpcloud_account_key)
-url_company_earnings_surprises = "https://fmpcloud.io/api/v3/earnings-surpises/%s?apikey=%s"  % (ticker,fmpcloud_account_key)
-url_company_sec_filings = "https://fmpcloud.io/api/v3/financial-statements/%s?datatype=zip&apikey=%s" % (ticker,fmpcloud_account_key)
-
-page = get_page(url_finviz)
-
-soup = BeautifulSoup(page.content, 'html.parser')
-
-table = soup.find_all('table')
-table_rows = table[8].find_all('tr', recursive=False)
-
-emptyDict = {}
-
-#Get rows of data.
-for tr in table_rows:
-    tds = tr.find_all('td')
-    boolKey = True
-    keyValueSet = False
-    for td in tds:
-        if boolKey:
-            key = td.text.strip()
-            boolKey = False                
-        else:
-            value = td.text.strip()
-            boolKey = True
-            keyValueSet = True                
-
-        if keyValueSet:
-            emptyDict[key] = value
-            keyValueSet = False
-
-df_company_details.loc[df_company_details['TICKER'] == ticker, 'PE'] = emptyDict['P/E']
-df_company_details.loc[df_company_details['TICKER'] == ticker, 'EPS_TTM'] = emptyDict['EPS (ttm)']
-df_company_details.loc[df_company_details['TICKER'] == ticker, 'PE_FORWARD'] = emptyDict['Forward P/E']
-df_company_details.loc[df_company_details['TICKER'] == ticker, 'EPS_Y1'] = emptyDict['EPS next Y']
-df_company_details.loc[df_company_details['TICKER'] == ticker, 'PEG'] = emptyDict['PEG']
-df_company_details.loc[df_company_details['TICKER'] == ticker, 'EPS_Y0'] = emptyDict['EPS this Y']
-df_company_details.loc[df_company_details['TICKER'] == ticker, 'PRICE_BOOK'] = emptyDict['P/B']
-df_company_details.loc[df_company_details['TICKER'] == ticker, 'PRICE_BOOK'] = emptyDict['P/B']
-df_company_details.loc[df_company_details['TICKER'] == ticker, 'PRICE_SALES'] = emptyDict['P/S']
-df_company_details.loc[df_company_details['TICKER'] == ticker, 'TARGET_PRICE'] = emptyDict['Target Price']
-df_company_details.loc[df_company_details['TICKER'] == ticker, 'ROE'] = emptyDict['ROE']
-df_company_details.loc[df_company_details['TICKER'] == ticker, '52W_RANGE'] = emptyDict['52W Range']
-df_company_details.loc[df_company_details['TICKER'] == ticker, 'QUICK_RATIO'] = emptyDict['Quick Ratio']
-df_company_details.loc[df_company_details['TICKER'] == ticker, 'GROSS_MARGIN'] = emptyDict['Gross Margin']
-df_company_details.loc[df_company_details['TICKER'] == ticker, 'CURRENT_RATIO'] = emptyDict['Current Ratio']
-
-import pdb; pdb.set_trace()
+#Sources:
 #https://finance.yahoo.com/
 #https://www.reuters.com
 #https://www.marketwatch.com/
 #https://www.marketscreener.com/
 #https://stockrow.com/
 
+#################
+ticker = "AAPL" # COMPANY TICKER - CHANGE HERE
+#################
 
+fmpcloud_account_key = '14afe305132a682a2742743df532707d'
+nasdaq_data_api_key = "u4udsfUDYFey58cp_4Gg"
 
-import pdb; pdb.set_trace()
+#Dates
+todays_date = date.today()
+one_year_ago = dt(todays_date.year - 1, 12, 31)
+two_year_ago = dt(todays_date.year - 2, 12, 31)
+three_year_ago = dt(todays_date.year - 3, 12, 31)
+list_dates = []
+list_dates.append(one_year_ago)
+list_dates.append(two_year_ago)
+list_dates.append(three_year_ago)
 
-"""
-for index, row in df_us_companies.iterrows():
-    print("Downloading Data For: %s - %s" % (row['TICKER'], row['COMPANY_NAME']))
+def return_tr_as_df(table_rows):
+    df = pd.DataFrame()
+    index = 0
+    #Get rows of data.
+    for tr in table_rows:
 
-    url_company_profile = "https://fmpcloud.io/api/v3/profile/%s?apikey=%s" % (row['TICKER'],nasdaq_data_api_key)
-    data_company_profile = get_api_json_data_no_file(url_company_profile)
-
-    df_us_companies.loc[df_us_companies['TICKER'] == data_company_profile[0][0]['symbol'], 'DESCRIPTION'] = data_company_profile[0][0]['description']
-
-    url_company_sec_filings = "https://fmpcloud.io/api/v3/financial-statements/%s?datatype=zip&apikey=%s" % (row['TICKER'],nasdaq_data_api_key)
-
-    
-    for index in data_company_profile:   
-        df_us_companies = df_us_companies.append({"DATE": dt.strptime(index['date'],"%Y-%m-%d"), "TICKER": row['sector'],"PE": pd.to_numeric(index['pe'])}, ignore_index=True)
-
-
-    url_company_key_metrics = "https://fmpcloud.io/api/v3/key-metrics-ttm/CRM?limit=40&apikey=14afe305132a682a2742743df532707d"
-    url_company_peers = "https://fmpcloud.io/api/v4/stock_peers?symbol=CRM&apikey=14afe305132a682a2742743df532707d"
-    url_company_earnings_surprises = "https://fmpcloud.io/api/v3/earnings-surpises/AAPL?apikey=14afe305132a682a2742743df532707d"
-
-    url_finviz = "https://finviz.com/quote.ashx?t=%s" % (row['TICKER'])
-    try:
-        page = get_page(url_finviz)
-
-        soup = BeautifulSoup(page.content, 'html.parser')
-
-        table = soup.find_all('table')
-        table_rows = table[8].find_all('tr', recursive=False)
-
-        emptyDict = {}
-
-        #Get rows of data.
-        for tr in table_rows:
-            tds = tr.find_all('td')
-            boolKey = True
-            keyValueSet = False
-            for td in tds:
-                if boolKey:
-                    key = td.text.strip()
-                    boolKey = False                
-                else:
-                    value = td.text.strip()
-                    boolKey = True
-                    keyValueSet = True                
-
-                if keyValueSet:
-                    emptyDict[key] = value
-                    keyValueSet = False
-
-        df_us_companies.loc[df_us_companies['TICKER'] == row['TICKER'], 'PRICE_BOOK'] = emptyDict['P/B']
-        df_us_companies.loc[df_us_companies['TICKER'] == row['TICKER'], 'PRICE_SALES'] = emptyDict['P/S']
-        df_us_companies.loc[df_us_companies['TICKER'] == row['TICKER'], '52W_RANGE'] = emptyDict['52W Range']
-    except:
-        print("Exception: %s - %s" % (row['TICKER'],row['COMPANY_NAME']))
-        pass
-    """
-
-import pdb; pdb.set_trace()
-
-df_company_list = pd.DataFrame()
-
-url = "https://fmpcloud.io/api/v3/stock-screener?&marketCapMoreThan=3000000000&exchange=NYSE,NASDAQ&isActivelyTrading=true&isEtf=false&apikey=%s" % (fmpcloud_account_key)
-data_turnover_over_3m = get_api_json_data(url,'030_stocks_turnover_over_3m.json')
-
-for index in data_turnover_over_3m:   
-    if(index['exchangeShortName'] in ['NASDAQ', 'NYSE'] and index['sector'] != ''):
-        df_company_list = df_company_list.append({"TICKER": index['symbol'], 
-            "COMPANY": index['companyName'],"MARKET_CAP": pd.to_numeric(index['marketCap']),
-            "SECTOR": index['sector'],"INDUSTRY": index['industry'],"BETA": pd.to_numeric(index['beta']),
-            "LAST": pd.to_numeric(index['price']),"LAST_DIVIDEND": pd.to_numeric(index['lastAnnualDividend']),"EXCHANGE": index['exchange'],"VOLUME": pd.to_numeric(index['volume'])}, ignore_index=True)
+        key = tr.th.text.strip()
         try:
-            url_finviz = "https://finviz.com/quote.ashx?t=%s" % (index['symbol'])
-            page = get_page(url_finviz)
-        except:
-            try:
-                url_finviz = "https://finviz.com/quote.ashx?t=%s" % (index['symbol'].split('-')[0])
-                page = get_page(url_finviz)
+            value = tr.td.text.strip()
+        except AttributeError as e:
+            value = ""
 
-                soup = BeautifulSoup(page.content, 'html.parser')
+        if(value):
+            df.insert(index,key,[value],True)
+            index+=1
+    return df
 
-                table = soup.find_all('table')
-                table_rows = table[8].find_all('tr', recursive=False)
+temp_excel_file_path = '/Trading_Excel_Files/04_Filtering_Process/030_Filtering_Process_Quantitative_Analysis_US_Stocks.xlsm'
+temp_sheet_name = 'Database US Companies'
 
-                temp_row = []
-                emptyDict = {}
+#Get company data from various sources
+df_us_companies = convert_excelsheet_to_dataframe(temp_excel_file_path, temp_sheet_name, False)
+df_zacks_stock_data = df_company_details = df_us_companies.loc[df_us_companies['TICKER'] == ticker].reset_index(drop=True)
+df_finwiz_stock_data = get_finwiz_stock_data(ticker)
 
-                #Get rows of data.
-                for tr in table_rows:
-                    tds = tr.find_all('td')
-                    boolKey = True
-                    keyValueSet = False
-                    for td in tds:
-                        if boolKey:
-                            key = td.text.strip()
-                            boolKey = False                
-                        else:
-                            value = td.text.strip()
-                            boolKey = True
-                            keyValueSet = True                
+url_nasdaq = "https://www.nasdaq.com/market-activity/stocks/%s/price-earnings-peg-ratios" % (ticker)
+page = get_page_selenium(url_nasdaq)
+soup = BeautifulSoup(page, 'html.parser')
 
-                        if keyValueSet:
-                            emptyDict[key] = value
-                            keyValueSet = False
+table = soup.find_all('table')
+pe_ratio_table_rows = table[0].find_all('tr', recursive=True)
 
-                df_company_list.loc[df_company_list['TICKER'] == index['symbol'], 'PE'] = emptyDict['P/E']
-                df_company_list.loc[df_company_list['TICKER'] == index['symbol'], 'EPS_TTM'] = emptyDict['EPS (ttm)']
-                df_company_list.loc[df_company_list['TICKER'] == index['symbol'], 'PE_FORWARD'] = emptyDict['Forward P/E']
-                df_company_list.loc[df_company_list['TICKER'] == index['symbol'], 'EPS_Y1'] = emptyDict['EPS next Y']
-                df_company_list.loc[df_company_list['TICKER'] == index['symbol'], 'PEG'] = emptyDict['PEG']
-                df_company_list.loc[df_company_list['TICKER'] == index['symbol'], 'EPS_Y0'] = emptyDict['EPS this Y']
-                df_company_list.loc[df_company_list['TICKER'] == index['symbol'], 'PRICE_BOOK'] = emptyDict['P/B']
-                df_company_list.loc[df_company_list['TICKER'] == index['symbol'], 'PRICE_BOOK'] = emptyDict['P/B']
-                df_company_list.loc[df_company_list['TICKER'] == index['symbol'], 'PRICE_SALES'] = emptyDict['P/S']
-                df_company_list.loc[df_company_list['TICKER'] == index['symbol'], 'TARGET_PRICE'] = emptyDict['Target Price']
-                df_company_list.loc[df_company_list['TICKER'] == index['symbol'], 'ROE'] = emptyDict['ROE']
-                df_company_list.loc[df_company_list['TICKER'] == index['symbol'], '52W_RANGE'] = emptyDict['52W Range']
-                df_company_list.loc[df_company_list['TICKER'] == index['symbol'], 'QUICK_RATIO'] = emptyDict['Quick Ratio']
-                df_company_list.loc[df_company_list['TICKER'] == index['symbol'], 'GROSS_MARGIN'] = emptyDict['Gross Margin']
-                df_company_list.loc[df_company_list['TICKER'] == index['symbol'], 'CURRENT_RATIO'] = emptyDict['Current Ratio']
-            except:
-                print("Exception: %s - %s" % (index['symbol'],index['companyName']))
-                pass
-    """
-    url_nasdaq = "https://www.nasdaq.com/market-activity/stocks/%s/price-earnings-peg-ratios" % (index['symbol'])
-    page1 = get_page_selenium(url_nasdaq)
-    soup1 = BeautifulSoup(page1, 'html.parser')
+df_pe_ratios = return_tr_as_df(pe_ratio_table_rows)
 
-    table1 = soup1.find_all('table')
-    table_rows1 = table1[0].find_all('tr', recursive=True)
-    https://fmpcloud.io/api/v3/ratios/AAPL?limit=40&apikey=14afe305132a682a2742743df532707d
-    url = "https://finviz.com/quote.ashx?t=%s"
-    """
+df_nasdaq_company_data = pd.DataFrame()
+df_nasdaq_company_data.loc[ticker, 'PE_F0-1_ACTUAL'] = df_pe_ratios.iloc[0,0]
+df_nasdaq_company_data.loc[ticker, 'PE_F0_ESTIMATE'] = df_pe_ratios.iloc[0,1]
+df_nasdaq_company_data.loc[ticker, 'PE_F1_ESTIMATE'] = df_pe_ratios.iloc[0,2]
+df_nasdaq_company_data.loc[ticker, 'PE_F2_ESTIMATE'] = df_pe_ratios.iloc[0,3]
 
-import pdb; pdb.set_trace()
+#fmpcloud urls:
+url_company_profile = "https://fmpcloud.io/api/v3/profile/%s?apikey=%s" % (ticker,fmpcloud_account_key)
+url_company_key_metrics = "https://fmpcloud.io/api/v3/key-metrics-ttm/%s?limit=40&apikey=%s"  % (ticker,fmpcloud_account_key)
+url_company_peers = "https://fmpcloud.io/api/v4/stock_peers?symbol=%s&apikey=%s"  % (ticker,fmpcloud_account_key)
+url_company_earnings_surprises = "https://fmpcloud.io/api/v3/earnings-surpises/%s?apikey=%s"  % (ticker,fmpcloud_account_key)
+url_company_sec_filings = "https://fmpcloud.io/api/v3/financial-statements/%s?datatype=zip&apikey=%s" % (ticker,fmpcloud_account_key)
+url_company_ratios = "https://fmpcloud.io/api/v3/ratios/%s?limit=40&apikey=%s" % (ticker,fmpcloud_account_key)
+url_company_income_statement = "https://fmpcloud.io/api/v3/income-statement/%s?limit=120&apikey=%s" % (ticker,fmpcloud_account_key)
+url_company_balance_sheet = "https://fmpcloud.io/api/v3/balance-sheet-statement/%s?limit=120&apikey=%s" % (ticker,fmpcloud_account_key)
+url_company_cash_flow_statement = "https://fmpcloud.io/api/v3/cash-flow-statement/%s?limit=120&apikey=1%s" % (ticker,fmpcloud_account_key)
+url_company_financial_growth = "https://fmpcloud.io/api/v3/financial-growth/%s?limit=20&apikey=%s" % (ticker,fmpcloud_account_key)
 
 """
+#nasdaq urls:
 url = "https://data.nasdaq.com/api/v3/datatables/ZACKS/EE.json?api_key=%s" % (nasdaq_data_api_key)
-data_earnings_estimates = get_api_json_data(url,'030_stock_earnings_estimates.json')
+data_earnings_estimates = get_api_json_data_no_file(url)
 
 url = "https://data.nasdaq.com/api/v3/datatables/ZACKS/SE.json?api_key=%s" % (nasdaq_data_api_key)
-data_sales_estimates = get_api_json_data(url,'030_stock_sales_estimates.json')
+data_sales_estimates = get_api_json_data_no_file(url)
 
 url = "https://data.nasdaq.com/api/v3/datatables/ZACKS/FC.json?api_key=%s" % (nasdaq_data_api_key)
-data_fundamentals = get_api_json_data(url,'030_stock_fundamentals.json')
+data_fundamentals = get_api_json_data_no_file(url)
 
 url = "https://data.nasdaq.com/api/v3/datatables/ZACKS/ES.json?api_key=%s" % (nasdaq_data_api_key)
-data_earnings_surprises = get_api_json_data(url,'030_stock_earnings_surprises.json')
+data_earnings_surprises = get_api_json_data_no_file(url)
 """
 
+#Excel file where we will create our one pager
+excel_file_path = '/Trading_Excel_Files/04_Filtering_Process/030_Filtering_Process_Quantitative_Analysis_US_StocksOnePager.xlsm'
+
+print(df_zacks_stock_data)
+print(df_finwiz_stock_data)
+print(df_nasdaq_company_data)
+
 import pdb; pdb.set_trace()
-#Companies over 3 billion turnover: https://fmpcloud.io/api/v3/stock-screener?&marketCapMoreThan=3000000000&apikey=14afe305132a682a2742743df532707d
-#Finwiz company quote: https://finviz.com/quote.ashx?t=CRM
-#Income Statement: https://fmpcloud.io/api/v3/income-statement/AAPL?limit=120&apikey=14afe305132a682a2742743df532707d
-#Balance Sheet: https://fmpcloud.io/api/v3/balance-sheet-statement/AAPL?limit=120&apikey=14afe305132a682a2742743df532707d
-#Cash Flow Statement: https://fmpcloud.io/api/v3/cash-flow-statement/AAPL?limit=120&apikey=14afe305132a682a2742743df532707d
-#Ratios: https://fmpcloud.io/api/v3/ratios/AAPL?limit=40&apikey=14afe305132a682a2742743df532707d
-
-# Forward Estimates Ratios: https://www.nasdaq.com/market-activity/stocks/aapl/price-earnings-peg-ratios
-
-#TODO: Convert the above json files into a dataframe
-
-#Winners and Losers:
-# https://fmpcloud.io/api/v3/actives?apikey=14afe305132a682a2742743df532707d
-# https://fmpcloud.io/api/v3/losers?apikey=14afe305132a682a2742743df532707d
-# https://fmpcloud.io/api/v3/gainers?apikey=14afe305132a682a2742743df532707d
-# https://fmpcloud.io/api/v3/sectors-performance?apikey=14afe305132a682a2742743df532707d
-
-# Finwiz company quote: https://finviz.com/quote.ashx?t=CRM
-# Company Real Time Quote: https://fmpcloud.io/api/v3/quote/AAPL?apikey=14afe305132a682a2742743df532707d
-# Company Profile: https://fmpcloud.io/api/v3/profile/CRM?apikey=14afe305132a682a2742743df532707d
-# Company Key Metrics: https://fmpcloud.io/api/v3/key-metrics-ttm/CRM?limit=40&apikey=14afe305132a682a2742743df532707d
-# Financial Statements Growth: https://fmpcloud.io/api/v3/financial-growth/CRM?limit=20&apikey=14afe305132a682a2742743df532707d
-# Company Earnings Call Transcripts: https://fmpcloud.io/api/v3/earning_call_transcript/CRM?quarter=3&year=2020&apikey=14afe305132a682a2742743df532707d
-# Company Earnings Surprises: https://fmpcloud.io/api/v3/earnings-surpises/AAPL?apikey=14afe305132a682a2742743df532707d
-# Company Peer List: https://fmpcloud.io/api/v4/stock_peers?symbol=CRM&apikey=14afe305132a682a2742743df532707d
 
 """
 Company Name
