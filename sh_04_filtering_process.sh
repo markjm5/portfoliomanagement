affacd6fc3a5ab4db6a8c6ab8b021ef47f6cc571
markjm5 <mark.jm5@gmail.com>
2022-07-02 23:31:49 +0800

    updated bash scripts to include 024 and 030

---
+++ sh_04_filtering_process.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo $(date) 
+echo "**********************************************"
+echo "* Started 024_Filtering_Process_ETF_Universe *"
+echo "**********************************************"
+python ~/Documents/PythonProjects/PortfolioManagement/024_Filtering_Process_ETF_Universe.py
+
+echo $(date) 
+echo "**********************************************"
+echo "* Started 024_Filtering_Process_ETF_Universe *"
+echo "**********************************************"
+python ~/Documents/PythonProjects/PortfolioManagement/030_Filtering_Process_US_Stocks.py
