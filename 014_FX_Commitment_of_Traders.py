b552da02cff8d4c8997607a119b85686351c8593
markjm5 <mark.jm5@gmail.com>
2022-07-22 00:14:54 +0800

    added initial 014 commitment of traders py file

--- 014_FX_Commitment_of_Traders.py
+++ 014_FX_Commitment_of_Traders.py
@@ -1,23 +1,20 @@
 import pandas as pd
-from datetime import datetime as dt
-from datetime import date
-from bs4 import BeautifulSoup
-from requests.models import parse_header_links
-from common import get_oecd_data, convert_excelsheet_to_dataframe, write_dataframe_to_excel
-from common import combine_df_on_index, get_stlouisfed_data, get_page
+from common import convert_excelsheet_to_dataframe, write_dataframe_to_excel
+from common import combine_df_on_index
 
 excel_file_path = '/Trading_Excel_Files/02_Interest_Rates_FX/014_FX_Commitment_of_Traders.xlsm'
 sheet_name = 'Database'
 
 df_original = convert_excelsheet_to_dataframe(excel_file_path, sheet_name, True)
 
-def return_df(df, value,ticker):
-    df = df.loc[df['CONTRACT_UNITS'].isin([value])].reset_index(drop=True)
-    df = df.drop(['NON_COMMERCIAL_POSITIONS_LONG', 'NON_COMMERCIAL_POSITIONS_SHORT', 'CONTRACT_UNITS'], axis=1)
+def return_df(df, search_str,ticker):
+    print("Getting Data For: %s" % (ticker))
+    df = df[df.CONTRACT_UNITS.str.contains(search_str,case=False)].groupby(['DATE']).sum().reset_index(drop=False)
+    #df = df.drop(['NON_COMMERCIAL_POSITIONS_LONG', 'NON_COMMERCIAL_POSITIONS_SHORT', 'CONTRACT_UNITS'], axis=1)
+    df = df.drop(['NON_COMMERCIAL_POSITIONS_LONG', 'NON_COMMERCIAL_POSITIONS_SHORT'], axis=1)
     df = df.rename(columns={"NON_COMMERCIAL_POSITIONS_NET": ticker})
 
     return df
-
 
 data_sheet_raw = 'Futures Only Report Raw'
 df_data_raw = convert_excelsheet_to_dataframe(excel_file_path, data_sheet_raw, False)
@@ -34,17 +31,21 @@ df_data_raw['NON_COMMERCIAL_POSITIONS_NET'] = df_data_raw['NON_COMMERCIAL_POSITI
 #TODO: Write updated df_original back to Database sheet
 #df_cols = df_updated.groupby(['CONTRACT_UNITS']).count()
 
-df_data_aud = return_df(df_data_raw,"(CONTRACTS OF AUD 100,000)","AUD")
-df_data_brl = return_df(df_data_raw,"(CONTRACTS OF BRL 100,000)","BRL")
-df_data_cad = return_df(df_data_raw,"(CONTRACTS OF CAD 100,000)","CAD")
-df_data_chf = return_df(df_data_raw,"(CONTRACTS OF CHF 125,000)","CHF")
-df_data_eur = return_df(df_data_raw,"(CONTRACTS OF EUR 125,000)","EUR")
-df_data_gbp = return_df(df_data_raw,"(CONTRACTS OF GBP 62,500)","GBP")
-df_data_jpy = return_df(df_data_raw,"(CONTRACTS OF JPY 12,500,000)","JPY")
-df_data_mxn = return_df(df_data_raw,"(CONTRACTS OF MXN 500,000)","MEX")
-df_data_nzd = return_df(df_data_raw,"(CONTRACTS OF NZD 100,000)","NZD")
-df_data_rub = return_df(df_data_raw,"(CONTRACTS OF RUB 2,500,000)","RUB")
-df_data_zar = return_df(df_data_raw,"(CONTRACTS OF ZAR 500,000)","ZAR")
+df_data_aud = return_df(df_data_raw,"CONTRACTS OF AUD","AUD")
+df_data_brl = return_df(df_data_raw,"CONTRACTS OF BRL","BRL")
+df_data_cad = return_df(df_data_raw,"CONTRACTS OF CAD","CAD")
+df_data_chf = return_df(df_data_raw,"CONTRACTS OF CHF","CHF")
+df_data_eur = return_df(df_data_raw,"CONTRACTS OF EUR","EUR")
+df_data_gbp = return_df(df_data_raw,"CONTRACTS OF GBP","GBP")
+df_data_jpy = return_df(df_data_raw,"CONTRACTS OF JPY","JPY")
+df_data_mxn = return_df(df_data_raw,"CONTRACTS OF MXN","MEX")
+df_data_nzd = return_df(df_data_raw,"CONTRACTS OF NZD","NZD")
+df_data_rub = return_df(df_data_raw,"CONTRACTS OF RUB","RUB")
+df_data_zar = return_df(df_data_raw,"CONTRACTS OF ZAR","ZAR")
+
+#pd.set_option('display.max_rows', None)
+#df_data_raw.groupby(['CONTRACT_UNITS']).count()
+#df_data_raw.loc[df_data_raw['CONTRACT_UNITS'].isin(["(CONTRACTS OF EUR 125,000)"])].reset_index(drop=True)
 
 df_new = combine_df_on_index(df_data_eur, df_data_cad, 'DATE')
 df_new = combine_df_on_index(df_new, df_data_mxn, 'DATE')
@@ -57,9 +58,9 @@ df_new = combine_df_on_index(df_new, df_data_gbp, 'DATE')
 df_new = combine_df_on_index(df_new, df_data_rub, 'DATE')
 df_new = combine_df_on_index(df_new, df_data_brl, 'DATE')
 
-#TODO: Fix issue with doubling of rows
+#TODO: Double check numbers, especially why CHF keeps having added a new row?
 
-import pdb; pdb.set_trace()
+df_updated = combine_df_on_index(df_original, df_new, 'DATE')
 
 # Write the updated df back to the excel sheet
 write_dataframe_to_excel(excel_file_path, sheet_name, df_updated, False, 0)
