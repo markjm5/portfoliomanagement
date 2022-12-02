6156e64b21cabd50df80db8eb7668f274535fe91
markjm5 <mark.jm5@gmail.com>
2022-12-02 22:36:47 +0800

    added packaging and distribution script

---
+++ 039_Package_And_Distribute.py
@@ -0,0 +1,42 @@
+from zipfile import ZipFile
+from datetime import date, datetime
+import os
+from os.path import basename
+
+# Zip the files from given directory that matches the filter
+def zipFilesInDir(dirName, zipFileName, filter):
+   # create a ZipFile object
+   with ZipFile(zipFileName, 'w') as zipObj:
+       # Iterate over all the files in directory
+       for folderName, subfolders, filenames in os.walk(dirName):
+           for filename in filenames:
+               if filter(filename):
+                   # create complete filepath of file in directory
+                   filePath = os.path.join(folderName, filename)
+                   # Add file to zip
+                   zipObj.write(filePath, basename(filePath))
+def main():
+    # Name of the Directory to be zipped
+    dirName = 'Trading_Excel_Files'
+
+    #get date range
+    todays_date = date.today()
+
+    HOUR = datetime.now().hour   # the current hour
+    MINUTE = datetime.now().minute # the current minute
+    SECONDS = datetime.now().second
+
+    file_name = "Trading_Excel_Files-%s-%s-%s-%s-%s-%s" % (todays_date.year, todays_date.month, todays_date.day, HOUR, MINUTE, SECONDS)
+
+    # create a ZipFile object
+    with ZipFile('sampleDir.zip', 'w') as zipObj:
+       # Iterate over all the files in directory
+       for folderName, subfolders, filenames in os.walk(dirName):
+           for filename in filenames:
+               #create complete filepath of file in directory
+               filePath = os.path.join(folderName, filename)
+               # Add file to zip
+               zipObj.write(filePath)
+
+if __name__ == '__main__':
+   main()
--- sh_04_filtering_process.sh
+++ sh_04_filtering_process.sh
@@ -10,10 +10,16 @@ python ~/Documents/PythonProjects/PortfolioManagement/024_Filtering_Process_ETF_
 #echo "********************************************"
 #echo "* Started 030_Filtering_Process_US_Stocks *"
 #echo "********************************************"
-#python ~/Documents/PythonProjects/PortfolioManagement/030_Filtering_Process_US_Stocks.py
+python ~/Documents/PythonProjects/PortfolioManagement/030_Filtering_Process_US_Stocks.py
 
 echo $(date) 
 echo "***************************************************"
 echo "* Started 031_Filtering_Idea_Generation_Bottom_Up *"
 echo "***************************************************"
 python ~/Documents/PythonProjects/PortfolioManagement/031_Filtering_Process_Idea_Generation_Bottom_Up.py
+
+echo $(date) 
+echo "**************************************"
+echo "* Started 039_Package_And_Distribute *"
+echo "**************************************"
+python ~/Documents/PythonProjects/PortfolioManagement/039_Package_And_Distribute.py
