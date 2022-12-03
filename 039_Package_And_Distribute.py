e89e79f5fecd187bcd4af5c71f96311d31a2e86f
markjm5 <mark.jm5@gmail.com>
2022-12-03 09:55:23 +0800

    added code to upload zip file to google drive

--- 039_Package_And_Distribute.py
+++ 039_Package_And_Distribute.py
@@ -1,10 +1,24 @@
 from zipfile import ZipFile
 from datetime import date, datetime
 import os
+import sys
 from os.path import basename
+from google_services import Create_Service
+from googleapiclient.http import MediaFileUpload
+
+CLIENT_SECRET_FILE = "google_client_secret.json"
+API_NAME = "drive"
+API_VERSION = "v3"
+SCOPES = ["https://www.googleapis.com/auth/drive"]
+
+service = Create_Service(CLIENT_SECRET_FILE,API_NAME,API_VERSION,SCOPES)
+
+google_drive_folder_id = "1-hfhZ9phouewa36YVU67x2h8Q8lkXzB6"
+mime_type = "application/zip"
 
 # Zip the files from given directory that matches the filter
 def zipFilesInDir(dirName, zipFileName, filter):
+
    # create a ZipFile object
    with ZipFile(zipFileName, 'w') as zipObj:
        # Iterate over all the files in directory
@@ -26,10 +40,10 @@ def main():
     MINUTE = datetime.now().minute # the current minute
     SECONDS = datetime.now().second
 
-    file_name = "Trading_Excel_Files-%s-%s-%s-%s-%s-%s" % (todays_date.year, todays_date.month, todays_date.day, HOUR, MINUTE, SECONDS)
-
+    file_name = "Trading_Excel_Files-%s-%s-%s-%s-%s-%s.zip" % (todays_date.year, todays_date.month, todays_date.day, HOUR, MINUTE, SECONDS)
+    file_path = "%s/../../../Desktop/%s" % (sys.path[0],file_name)
     # create a ZipFile object
-    with ZipFile(file_name + '.zip', 'w') as zipObj:
+    with ZipFile(file_path, 'w') as zipObj:
        # Iterate over all the files in directory
        for folderName, subfolders, filenames in os.walk(dirName):
            for filename in filenames:
@@ -37,6 +51,19 @@ def main():
                filePath = os.path.join(folderName, filename)
                # Add file to zip
                zipObj.write(filePath)
+
+    file_metadata = {
+        "name": file_name,
+        "parents": [google_drive_folder_id]
+    }
+
+    media = MediaFileUpload(file_path,mimetype=mime_type)
+
+    service.files().create(
+        body=file_metadata,
+        media_body=media,
+        fields='id'
+    ).execute()
 
 if __name__ == '__main__':
    main()
