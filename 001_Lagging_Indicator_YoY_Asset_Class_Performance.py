81066bfc9ea5e804405f76369ff30f4c15a1f4ba
markjm5 <mark.jm5@gmail.com>
2022-12-24 01:45:00 +0800

    added script to run all scripts

--- 001_Lagging_Indicator_YoY_Asset_Class_Performance.py
+++ 001_Lagging_Indicator_YoY_Asset_Class_Performance.py
@@ -3,73 +3,71 @@ from datetime import date
 from common import convert_excelsheet_to_dataframe, write_dataframe_to_excel
 from common import combine_df_on_index, get_yf_historical_stock_data
 
-if __name__ == "__main__":
+excel_file_path = '/Trading_Excel_Files/01_Lagging_Coincident_Indicators/001_Lagging_Indicator_YoY_Asset_Class_Performance.xlsm'
 
-    excel_file_path = '/Trading_Excel_Files/01_Lagging_Coincident_Indicators/001_Lagging_Indicator_YoY_Asset_Class_Performance.xlsm'
+####################################
+# Get Asset Class Data from YF.com #
+####################################
 
-    ####################################
-    # Get Asset Class Data from YF.com #
-    ####################################
+sheet_name = 'Database'
+df_original = convert_excelsheet_to_dataframe(excel_file_path, sheet_name, True, None,'%d/%m/%Y')
 
-    sheet_name = 'Database'
-    df_original = convert_excelsheet_to_dataframe(excel_file_path, sheet_name, True, None,'%d/%m/%Y')
+data = {'DATE': []}
 
-    data = {'DATE': []}
+# Convert the dictionary into DataFrame
+df_etf_data = pd.DataFrame(data)
 
-    # Convert the dictionary into DataFrame
-    df_etf_data = pd.DataFrame(data)
+#get date range
+todays_date = date.today()
+date_str = "%s-%s-%s" % (todays_date.year, todays_date.month, todays_date.day)
 
-    #get date range
-    todays_date = date.today()
-    date_str = "%s-%s-%s" % (todays_date.year, todays_date.month, todays_date.day)
+etfs = [ 'RXI','XLP','XLY','XLE','XLF','XLV','XLI','XLK','XLB','XLRE','XLC','XLU','SPY','USO','QQQ','IWM','IBB','EEM','HYG','VNQ','MDY','SLY','EFA','TIP','AGG','DJP','BIL','GC=F','DX-Y.NYB']
 
-    etfs = [ 'RXI','XLP','XLY','XLE','XLF','XLV','XLI','XLK','XLB','XLRE','XLC','XLU','SPY','USO','QQQ','IWM','IBB','EEM','HYG','VNQ','MDY','SLY','EFA','TIP','AGG','DJP','BIL','GC=F','DX-Y.NYB']
+for etf in etfs:
+    print("Getting Data For: %s" % (etf,))
+    df_etf = get_yf_historical_stock_data(etf, "1d", "2007-01-01", date_str)
 
-    for etf in etfs:
-        print("Getting Data For: %s" % (etf,))
-        df_etf = get_yf_historical_stock_data(etf, "1d", "2007-01-01", date_str)
+    #Remove unnecessary columns and rename columns
+    df_etf = df_etf.drop(['Open', 'High', 'Low', 'Volume'], axis=1)
+    df_etf = df_etf.rename(columns={"Close": etf})
 
-        #Remove unnecessary columns and rename columns
-        df_etf = df_etf.drop(['Open', 'High', 'Low', 'Volume'], axis=1)
-        df_etf = df_etf.rename(columns={"Close": etf})
+    df_etf_data = combine_df_on_index(df_etf_data, df_etf, 'DATE')
 
-        df_etf_data = combine_df_on_index(df_etf_data, df_etf, 'DATE')
+df_original = convert_excelsheet_to_dataframe(excel_file_path, sheet_name, True)
 
-    df_original = convert_excelsheet_to_dataframe(excel_file_path, sheet_name, True)
+df_updated = combine_df_on_index(df_original, df_etf_data, 'DATE')
 
-    df_updated = combine_df_on_index(df_original, df_etf_data, 'DATE')
-
-    # Write the updated df back to the excel sheet
-    write_dataframe_to_excel(excel_file_path, sheet_name, df_updated, False, 0)
+# Write the updated df back to the excel sheet
+write_dataframe_to_excel(excel_file_path, sheet_name, df_updated, False, 0)
 
 
-    #Calculate Annual Returns on each asset class. https://stackoverflow.com/questions/64096220/how-to-calculate-the-yearly-percent-return-in-pandas
-    sheet_name = 'DB Annual Returns'
+#Calculate Annual Returns on each asset class. https://stackoverflow.com/questions/64096220/how-to-calculate-the-yearly-percent-return-in-pandas
+sheet_name = 'DB Annual Returns'
 
-    data = {'DATE': []}
+data = {'DATE': []}
 
-    # Convert the dictionary into DataFrame
-    df_percentage_change = pd.DataFrame(data)
+# Convert the dictionary into DataFrame
+df_percentage_change = pd.DataFrame(data)
 
-    for etf in etfs:
+for etf in etfs:
 
-        #groupby year and determine the daily percent change by year, and add it as a column to df
-        df_etf_data['%s_pct_ch' % (etf,)] = df_etf_data.groupby(df_etf_data.DATE.dt.year, group_keys=False)[etf].apply(pd.Series.pct_change)
+    #groupby year and determine the daily percent change by year, and add it as a column to df
+    df_etf_data['%s_pct_ch' % (etf,)] = df_etf_data.groupby(df_etf_data.DATE.dt.year, group_keys=False)[etf].apply(pd.Series.pct_change)
 
-        #Drop unnecessary columns
-        df_etf_data = df_etf_data.drop(columns=etf, axis=1)
+    #Drop unnecessary columns
+    df_etf_data = df_etf_data.drop(columns=etf, axis=1)
 
-        # groupby year and aggregate sum of pct_ch to get the yearly return
-        #df_yearly_pct_ch = df_etf_data.groupby(df_etf_data.DATE.dt.year)['%s_pct_ch' % (etf,)].sum().mul(100).reset_index().rename(columns={'%s_pct_ch' % (etf,): etf})
-        df_yearly_pct_ch = df_etf_data.groupby(df_etf_data.DATE.dt.year, group_keys=False)['%s_pct_ch' % (etf,)].sum().reset_index().rename(columns={'%s_pct_ch' % (etf,): etf})
+    # groupby year and aggregate sum of pct_ch to get the yearly return
+    #df_yearly_pct_ch = df_etf_data.groupby(df_etf_data.DATE.dt.year)['%s_pct_ch' % (etf,)].sum().mul(100).reset_index().rename(columns={'%s_pct_ch' % (etf,): etf})
+    df_yearly_pct_ch = df_etf_data.groupby(df_etf_data.DATE.dt.year, group_keys=False)['%s_pct_ch' % (etf,)].sum().reset_index().rename(columns={'%s_pct_ch' % (etf,): etf})
 
-        df_percentage_change = combine_df_on_index(df_percentage_change, df_yearly_pct_ch, 'DATE')
+    df_percentage_change = combine_df_on_index(df_percentage_change, df_yearly_pct_ch, 'DATE')
 
-    df_original = convert_excelsheet_to_dataframe(excel_file_path, sheet_name, False)
+df_original = convert_excelsheet_to_dataframe(excel_file_path, sheet_name, False)
 
-    df_updated = combine_df_on_index(df_original, df_percentage_change, 'DATE')
+df_updated = combine_df_on_index(df_original, df_percentage_change, 'DATE')
 
-    # Write the updated df back to the excel sheet
-    write_dataframe_to_excel(excel_file_path, sheet_name, df_updated, False, -1)
+# Write the updated df back to the excel sheet
+write_dataframe_to_excel(excel_file_path, sheet_name, df_updated, False, -1)
 
-    print("Done!")
+print("Done!")
---
+++ sh_00_run_all_scripts.py
@@ -0,0 +1,37 @@
+import os
+from datetime import datetime
+
+
+def run_script(file_name):
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    print("current time = %s" % (current_time))
+    print("=================================================================================")
+    print("* Started %s *" % (file_name))
+    print("=================================================================================")
+
+    run_script("%s" % (file_name))
+
+
+run_script("001_Lagging_Indicator_YoY_Asset_Class_Performance.py")
+run_script("002_Lagging_Indicator_US_GDP.py")
+run_script("003_Lagging_Indicator_World_GDP.py")
+run_script("005_Lagging_Indicator_Job_Market_US_World.py")
+run_script("006_Lagging_Indicator_PCE.py")
+run_script("007_Lagging_Indicator_US_Inflation.py")
+run_script("008_Lagging_Indicator_Global_Inflation.py")
+run_script("009_Lagging_Indicator_US_Industrial_Production.py")
+#run_script("010_Lagging_Indicator_World_Industrial_Production.py")
+run_script("011_Lagging_Indicator_US_Durable_Goods.py")
+run_script("011_Lagging_Indicator_US_Retail_Sales.py")
+run_script("012_Central_Banks.py")
+run_script("014_US_Global_Money_Supply.py")
+run_script("015_Leading_Indicator_US_LEI_Consumer_Confidence.py")
+run_script("016_Leading_Indicator_US_ISM_Manufacturing.py")
+run_script("017_Leading_Indicator_US_ISM_Services.py")
+run_script("018_Leading_Indicator_PMI_Manufacturing_World.py")
+run_script("019_Leading_Indicator_PMI_Services_World.py")
+run_script("020_Leading_Indicator_US_Housing_Market.py")
+run_script("024_Filtering_Process_ETF_Universe.py")
+run_script("030_Filtering_Process_US_Stocks.py")
+run_script("031_Filtering_Process_Idea_Generation_Bottom_Up.py")
